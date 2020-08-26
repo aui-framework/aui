b2ad3f63720bd42945c80b0a792049b8705c4f53
Alex2772 <alex2772sc@gmail.com>
2020-08-26 20:52:04 +0300

    Переделаны ассеты в AUI.Example.Views

--- examples/AUI.Example.Views/src/main.cpp
+++ examples/AUI.Example.Views/src/main.cpp
@@ -1,17 +1,11 @@
 #include "ExampleWindow.h"
 #include "AUI/Common/ByteBuffer.h"
 #include "AUI/Util/BuiltinFiles.h"
-#include "assets.h"
 
 int main()
 {
     Stylesheet::setPreferredStyle(Stylesheet::PREFER_UNIVERSAL_LOOK);
-	{
-		BuiltinFiles f;
-		ByteBuffer b(AUI_PACKED_asset, sizeof(AUI_PACKED_asset));
-		f.loadBuffer(b);
-		Stylesheet::instance().load(f.open("assets/views/style.css"));
-	}
+    Stylesheet::instance().load(AUrl(":views/style.css").open());
 	ExampleWindow w;
 	w.loop();
 	return 0;
