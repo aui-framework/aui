979b6787a50c37dcbe60693cef1021c2c7c98434
Alex2772 <alex2772sc@gmail.com>
2020-08-30 03:28:20 +0300

    Добавлены недостающие файлы
    (cherry picked from commit d459156e483073c039a567c578b9a75571f24fc9)

---
+++ AUI.Core/src/AUI/Util/ARandom.cpp
@@ -0,0 +1,44 @@
+﻿#include "ARandom.h"
+#include <ctime>
+
+ARandom::ARandom()
+{
+	mRandom.seed(std::time(nullptr));
+}
+
+int ARandom::nextInt()
+{
+	return std::uniform_int_distribution<int>()(mRandom);
+}
+
+float ARandom::nextFloat()
+{
+	return std::uniform_real_distribution<float>()(mRandom);
+}
+
+float ARandom::nextFloat(float min, float max)
+{
+	return std::uniform_real_distribution<float>(min, max)(mRandom);
+}
+
+glm::vec2 ARandom::nextVec2()
+{
+	return { nextFloat(), nextFloat() };
+}
+
+glm::vec2 ARandom::nextVec2(float min, float max)
+{
+	return { nextFloat(min, max), nextFloat(min, max) };
+}
+
+_<ByteBuffer> ARandom::nextBytes(unsigned count)
+{
+	auto buf = _new<ByteBuffer>();
+	buf->reserve(count);
+	for (unsigned i = 0; i < count; ++i)
+	{
+		unsigned char c = std::uniform_int_distribution<unsigned>()(mRandom) % 0xff;
+		buf->put(reinterpret_cast<char*>(&c), 1);
+	}
+	return buf;
+}
