86aa2e181d5d1e8478fa97188544b5fcc1f61236
Alex2772 <alex2772sc@gmail.com>
2020-12-07 01:37:13 +0300

    Фикс инклюда AUI.Build.cmake

--- cmake/c.cmake
+++ cmake/c.cmake
@@ -5,3 +5,4 @@ get_filename_component(SELF_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
 include_directories(${SELF_DIR}/include)
 
 include(${SELF_DIR}/cmake/AUI.cmake)
+include(${SELF_DIR}/cmake/AUI.Build.cmake)
