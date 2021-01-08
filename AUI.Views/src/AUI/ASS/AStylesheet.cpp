097239cc089609dc82942ef565b59e6318ed19ce
Alex2772 <alex2772sc@gmail.com>
2021-01-09 00:47:13 +0300

    added divider styles

--- AUI.Views/src/AUI/ASS/AStylesheet.cpp
+++ AUI.Views/src/AUI/ASS/AStylesheet.cpp
@@ -35,6 +35,8 @@
 #include <AUI/View/ARadioButton.h>
 #include <AUI/View/AComboBox.h>
 #include <AUI/View/AListView.h>
+#include <AUI/View/AHDividerView.h>
+#include <AUI/View/AVDividerView.h>
 #include "AStylesheet.h"
 #include "ASS.h"
 
@@ -46,31 +48,31 @@ AStylesheet::AStylesheet() {
         // COMMON VIEWS ======================================
         // AView
         {
-                type_of<AView>(),
+            type_of<AView>(),
             FontSize {9_pt},
             FontRendering::SUBPIXEL,
         },
 
         // AWindow
         {
-                type_of<AWindow>(),
+            type_of<AWindow>(),
             BackgroundSolid {0xf0f0f0_rgb},
             Padding { 4_dp },
         },
 
         // ALabel
         {
-                type_of<ALabel>(),
+            type_of<ALabel>(),
             Padding {1_dp, 0, 2_dp},
         },
         {
-                type_of<ALabel>(),
+            type_of<ALabel>(),
             Margin {2_dp, 4_dp},
         },
 
         // AButton
         {
-                type_of<AButton>(),
+            type_of<AButton>(),
             BackgroundSolid {0xffffff_rgb},
             Padding {4_dp, 8_dp},
             Margin {2_dp, 4_dp},
@@ -80,15 +82,15 @@ AStylesheet::AStylesheet() {
             BoxShadow {{}, 1_dp, 4_dp, -2_dp, 0x80000000_argb},
         },
         {
-                type_of<AButton>::hover(),
+            type_of<AButton>::hover(),
             Border {1_dp, getOsThemeColor() * glm::vec4(1, 1, 1, 0.3f)}
         },
         {
-                type_of<AButton>::active(),
+            type_of<AButton>::active(),
             BackgroundSolid{0xfafafa_rgb},
         },
         {
-                type_of<AButton>()["default"],
+            type_of<AButton>()["default"],
             FontRendering::ANTIALIASING,
             BackgroundGradient {getOsThemeColor().lighter(0.15f),
                                 getOsThemeColor().darker(0.15f),
@@ -98,26 +100,26 @@ AStylesheet::AStylesheet() {
             TextColor { 0xffffff_rgb },
         },
         {
-                type_of<AButton>::hover()["default"],
+            type_of<AButton>::hover()["default"],
             BoxShadow { 0, 1_dp, 6_dp, -1_dp, getOsThemeColor() },
         },
         {
-                type_of<AButton>::active(),
+            type_of<AButton>::active(),
             Padding {5_dp, 8_dp, 3_dp},
             BoxShadow { nullptr },
         },
         {
-                type_of<AButton>::hover()["default"],
+            type_of<AButton>::hover()["default"],
             BackgroundGradient {getOsThemeColor().lighter(0.2f),
                                 getOsThemeColor().darker(0.15f),
                                 LayoutDirection::VERTICAL },
         },
         {
-                type_of<AButton>::active()["default"],
+            type_of<AButton>::active()["default"],
             BackgroundSolid { getOsThemeColor() }
         },
         {
-                type_of<AButton>()["disabled"],
+            type_of<AButton>()["disabled"],
             BackgroundSolid { 0xcccccc_rgb },
             BoxShadow { nullptr },
             Border {1_dp, 0xbfbfbf_rgb },
@@ -126,7 +128,7 @@ AStylesheet::AStylesheet() {
 
         // Text fields
         {
-                type_of<AAbstractTextField>(),
+            type_of<AAbstractTextField>(),
             Padding { 3_dp, 6_dp },
             ACursor::TEXT,
         },
@@ -158,7 +160,7 @@ AStylesheet::AStylesheet() {
             BackgroundSolid { 0xf0f0f0_rgb }
         },
         {
-                type_of<ANumberPicker>() >> type_of<AButton>(),
+            type_of<ANumberPicker>() >> type_of<AButton>(),
             Margin { 0 },
             Padding { 0 },
             FixedSize { 19_dp, {} },
@@ -167,15 +169,15 @@ AStylesheet::AStylesheet() {
 
         // ACheckBox
         {
-                type_of<ACheckBox>(),
+            type_of<ACheckBox>(),
             Margin { 1_dp, 4_dp },
         },
         {
-                type_of<ACheckBox>() > type_of<ALabel>(),
+            type_of<ACheckBox>() > type_of<ALabel>(),
             Margin { 1_dp, 4_dp },
         },
         {
-                type_of<ACheckBoxInner>(),
+            type_of<ACheckBoxInner>(),
             BackgroundSolid { 0xffffff_rgb },
             Margin { 1_dp, 1_dp, 1_dp, 4_dp },
             Border { 1_px, 0x333333_rgb },
@@ -183,26 +185,26 @@ AStylesheet::AStylesheet() {
             BackgroundImage {0x333333_rgb },
         },
         {
-                type_of<ACheckBoxInner>::hover(),
+            type_of<ACheckBoxInner>::hover(),
             Border { 1_px, 0x0078d7_rgb },
             BackgroundImage {0x0078d7_rgb },
         },
         {
-                type_of<ACheckBoxInner>::hover(),
+            type_of<ACheckBoxInner>::hover(),
             BackgroundSolid { 0xcce4f7_rgb },
             Border { 1_px, 0x005499_rgb },
             BackgroundImage {0x005499_rgb },
         },
         {
-                type_of<ACheckBox>()["checked"] > type_of<ACheckBoxInner>(),
+            type_of<ACheckBox>()["checked"] > type_of<ACheckBoxInner>(),
             BackgroundImage {":uni/svg/checkbox.svg" },
         },
         {
-                type_of<ACheckBox>()["disabled"] > type_of<ALabel>(),
+            type_of<ACheckBox>()["disabled"] > type_of<ALabel>(),
             TextColor { 0xa0a0a0_rgb },
         },
         {
-                type_of<ACheckBox>()["disabled"] > type_of<ACheckBoxInner>(),
+            type_of<ACheckBox>()["disabled"] > type_of<ACheckBoxInner>(),
             BackgroundSolid { 0xe5e5e5_rgb },
             BackgroundImage { 0xa0a0a0_rgb },
             Border { 1_px, 0xa0a0a0_rgb },
@@ -210,15 +212,15 @@ AStylesheet::AStylesheet() {
 
         // ARadioButton
         {
-                type_of<ARadioButton>(),
+            type_of<ARadioButton>(),
             Margin { 1_dp, 4_dp },
         },
         {
-                type_of<ARadioButton>() > type_of<ALabel>(),
+            type_of<ARadioButton>() > type_of<ALabel>(),
             Margin { 1_dp, 4_dp },
         },
         {
-                type_of<ARadioButtonInner>(),
+            type_of<ARadioButtonInner>(),
             BackgroundSolid { 0xffffff_rgb },
             Margin { 1_dp, 1_dp, 1_dp, 4_dp },
             Border { 1_px, 0x333333_rgb },
@@ -227,26 +229,26 @@ AStylesheet::AStylesheet() {
             BackgroundImage {0x333333_rgb },
         },
         {
-                type_of<ARadioButtonInner>::hover(),
+            type_of<ARadioButtonInner>::hover(),
             Border { 1_px, 0x0078d7_rgb },
             BackgroundImage {0x0078d7_rgb },
         },
         {
-                type_of<ARadioButtonInner>::hover(),
+            type_of<ARadioButtonInner>::hover(),
             BackgroundSolid { 0xcce4f7_rgb },
             Border { 1_px, 0x005499_rgb },
             BackgroundImage {0x005499_rgb },
         },
         {
-                type_of<ARadioButton>()["checked"] > type_of<ARadioButtonInner>(),
+            type_of<ARadioButton>()["checked"] > type_of<ARadioButtonInner>(),
             BackgroundImage {":uni/svg/radio.svg" },
         },
         {
-                type_of<ARadioButton>()["disabled"] > type_of<ALabel>(),
+            type_of<ARadioButton>()["disabled"] > type_of<ALabel>(),
             TextColor { 0xa0a0a0_rgb },
         },
         {
-                type_of<ARadioButton>()["disabled"] > type_of<ARadioButtonInner>(),
+            type_of<ARadioButton>()["disabled"] > type_of<ARadioButtonInner>(),
             BackgroundSolid { 0xe5e5e5_rgb },
             BackgroundImage { 0xa0a0a0_rgb },
             Border { 1_px, 0xa0a0a0_rgb },
@@ -254,30 +256,40 @@ AStylesheet::AStylesheet() {
 
         // AComboBox
         {
-                type_of<AComboBox>(),
+            type_of<AComboBox>(),
             TextAlign::LEFT,
         },
 
         // AListView
         {
-                type_of<AListView>(),
+            type_of<AListView>(),
             BackgroundSolid { 0xffffff_rgb },
             Border { 1_px, 0x828790_rgb },
             Padding { 2_px },
             Overflow::HIDDEN,
         },
         {
-                type_of<AListView>() > type_of<ALabel>(),
+            type_of<AListView>() > type_of<ALabel>(),
             Margin { 0 },
             Padding { 1_px, 4_px, 4_px },
         },
         {
-                type_of<AListView>() > type_of<ALabel>::hover(),
+            type_of<AListView>() > type_of<ALabel>::hover(),
             BackgroundSolid { 0xe5f3ff_rgb },
         },
         {
-                type_of<AListView>() > type_of<ALabel>()["selected"],
+            type_of<AListView>() > type_of<ALabel>()["selected"],
             BackgroundSolid { 0xcde8ff_rgb },
+        },
+
+        // ADividerView
+        {
+            type_of<AHDividerView>(),
+            FixedSize { {}, 1_dp },
+        },
+        {
+            type_of<AVDividerView>(),
+            FixedSize { 1_dp, {} },
         },
 
         // CUSTOM WINDOWS ===================================================
@@ -287,7 +299,7 @@ AStylesheet::AStylesheet() {
             FixedSize { {}, 30_dp }
         },
         {
-                class_of(".window-title") >> type_of<ALabel>(),
+            class_of(".window-title") >> type_of<ALabel>(),
             Margin { 0 },
             Padding { 7_dp },
             TextColor { 0xffffff_rgb }
@@ -311,11 +323,11 @@ AStylesheet::AStylesheet() {
             BackgroundImage {":uni/caption/close.svg" }
         },
         {
-                class_of(".window-title") >> type_of<AButton>::hover(),
+            class_of(".window-title") >> type_of<AButton>::hover(),
             BackgroundSolid { 0x20ffffff_argb },
         },
         {
-                class_of(".window-title") >> type_of<AButton>::active(),
+            class_of(".window-title") >> type_of<AButton>::active(),
             BackgroundSolid { 0x30ffffff_argb },
         },
         {
