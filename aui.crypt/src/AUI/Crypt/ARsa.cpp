0cf6b37f1dece5529c85f4713be3b70b5ee5a1db
alex2772-mi <alex2772@alex2772.ru>
2021-10-05 17:21:46 +0300

    added small dependencies to build tree
    Took 18 seconds

---
+++ aui.crypt/src/AUI/Crypt/ARsa.cpp
@@ -0,0 +1,143 @@
+/**
+ * =====================================================================================================================
+ * Copyright (c) 2021 Alex2772
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ 
+ * Original code located at https://github.com/aui-framework/aui
+ * =====================================================================================================================
+ */
+
+#include "ARsa.h"
+#include <openssl/rsa.h>
+#include <openssl/pem.h>
+#include "AUI/Common/AException.h"
+#include <glm/glm.hpp>
+
+
+unsigned char gKey[] = { 52, 12, 96, 112, 84, 4, 95, 125, 213, 234, 87, 195, 175, 13, 42, 0 };
+
+ARsa::ARsa(void* rsa): mRsa(rsa)
+{
+	if (!rsa)
+		throw AException("RSA cannot be nullptr");
+}
+
+ARsa::~ARsa()
+{
+	RSA_free(static_cast<RSA*>(mRsa));
+}
+
+AByteBuffer ARsa::encrypt(const AByteBuffer& in)
+{
+    AByteBuffer buf;
+	buf.reserve(in.getAvailable() + 0x1000);
+	while (in.getAvailable()) {
+		auto toRead = glm::min(in.getAvailable(), getKeyLength() - RSA_PKCS1_PADDING_SIZE);
+		int r = RSA_public_encrypt(toRead, reinterpret_cast<const unsigned char*>(in.getCurrentPosAddress()),
+			reinterpret_cast<unsigned char*>(buf.data() + buf.getSize()), static_cast<RSA*>(mRsa),
+			RSA_PKCS1_PADDING);
+
+		if (r < 0)
+			throw AException("Could not RSA_public_encrypt");
+
+		in.setCurrentPos(in.getCurrentPos() + toRead);
+		
+		buf.setSize(buf.getSize() + r);
+	}
+
+	return buf;
+}
+
+AByteBuffer ARsa::decrypt(const AByteBuffer& in)
+{
+    AByteBuffer buf;
+	buf.reserve(in.getAvailable() + 0x1000);
+	while (in.getAvailable()) {
+		auto toRead = glm::min(in.getAvailable(), getKeyLength());
+		int r = RSA_private_decrypt(toRead, reinterpret_cast<const unsigned char*>(in.getCurrentPosAddress()),
+			reinterpret_cast<unsigned char*>(buf.data() + buf.getSize()), static_cast<RSA*>(mRsa),
+			RSA_PKCS1_PADDING);
+		if (r < 0)
+			throw AException("Could not RSA_private_decrypt");
+
+		in.setCurrentPos(in.getCurrentPos() + toRead);
+		buf.setSize(buf.getSize() + r);
+	}
+	return buf;
+}
+
+size_t ARsa::getKeyLength() const
+{
+	return RSA_size(static_cast<RSA*>(mRsa));
+}
+
+AByteBuffer ARsa::getPrivateKeyPEM() const
+{
+    AByteBuffer byteBuffer;
+    BIO* bioKey = BIO_new(BIO_s_mem());
+    PEM_write_bio_RSAPrivateKey(bioKey, (RSA*)mRsa, EVP_des_ede3_cbc(), nullptr, 0, nullptr, gKey);
+	char* data;
+	long size = BIO_get_mem_data(bioKey, &data);
+
+	byteBuffer.put(data, size);
+	BIO_free(bioKey);
+
+	return byteBuffer;
+}
+AByteBuffer ARsa::getPublicKeyPEM() const
+{
+    AByteBuffer byteBuffer;
+	BIO* bioKey = BIO_new(BIO_s_mem());
+	PEM_write_bio_RSAPublicKey(bioKey, (RSA*)mRsa);
+
+	char* data;
+	long size = BIO_get_mem_data(bioKey, &data);
+
+	byteBuffer.put(data, size);
+	BIO_free(bioKey);
+
+	return byteBuffer;
+}
+
+_<ARsa> ARsa::generate(int bits)
+{
+	return _<ARsa>(new ARsa(RSA_generate_key(bits, RSA_F4, nullptr, nullptr)));
+}
+
+_<ARsa> ARsa::fromPrivateKeyPEM(const AByteBuffer& buffer)
+{
+	BIO* inputBuffer = BIO_new_mem_buf(buffer.getCurrentPosAddress(), buffer.getAvailable());
+	RSA* rsa = nullptr;
+	PEM_read_bio_RSAPrivateKey(inputBuffer, (RSA**)&rsa, nullptr, gKey);
+	BIO_free(inputBuffer);
+
+	if (rsa == nullptr)
+		throw AException("Could not create RSA private key");
+
+	return _<ARsa>(new ARsa(rsa));
+}
+
+_<ARsa> ARsa::fromPublicKeyPEM(const AByteBuffer& buffer)
+{
+	BIO* inputBuffer = BIO_new_mem_buf(buffer.getCurrentPosAddress(), buffer.getAvailable());
+	RSA* rsa = nullptr;
+	PEM_read_bio_RSAPublicKey(inputBuffer, (RSA**)&rsa, nullptr, nullptr);
+	BIO_free(inputBuffer);
+
+	if (rsa == nullptr)
+		throw AException("Could not create RSA public key");
+
+	return _<ARsa>(new ARsa(rsa));
+}
---
+++ aui.crypt/tests/Rsa.cpp
@@ -0,0 +1,88 @@
+/**
+ * =====================================================================================================================
+ * Copyright (c) 2021 Alex2772
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ 
+ * Original code located at https://github.com/aui-framework/aui
+ * =====================================================================================================================
+ */
+
+//
+// Created by alex2 on 31.08.2020.
+//
+
+#include <boost/test/unit_test.hpp>
+#include <AUI/Crypt/ARsa.h>
+#include <AUI/Util/ARandom.h>
+#include <AUI/Common/AString.h>
+
+using namespace boost::unit_test;
+
+
+BOOST_AUTO_TEST_SUITE(Rsa)
+
+    BOOST_AUTO_TEST_CASE(EncryptDecrypt) {
+        unsigned bits = 512;
+        for (uint32_t i = 0; i < 4; ++i, bits <<= 1) {
+            auto r = ARsa::generate();
+            for (auto size : {10, 1000, 5000}) {
+                auto someData = ARandom().nextBytes(size);
+
+                auto encrypted = r->encrypt(someData);
+                auto decrypted = r->decrypt(encrypted);
+                BOOST_CHECK_EQUAL(decrypted, someData);
+            }
+        }
+
+    }
+
+    BOOST_AUTO_TEST_CASE(PEM_PrivateWrite) {
+        auto r = ARsa::generate();
+        AString s = AString::fromLatin1(r->getPrivateKeyPEM());
+        BOOST_TEST(s.startsWith("-----BEGIN RSA PRIVATE KEY-----"));
+        BOOST_TEST(s.endsWith("-----END RSA PRIVATE KEY-----\n"));
+    }
+
+    BOOST_AUTO_TEST_CASE(PEM_PublicWrite) {
+        auto r = ARsa::generate();
+        AString s = AString::fromLatin1(r->getPublicKeyPEM());
+
+        BOOST_TEST(s.startsWith("-----BEGIN RSA PUBLIC KEY-----"));
+        BOOST_TEST(s.endsWith("-----END RSA PUBLIC KEY-----\n"));
+    }
+
+    BOOST_AUTO_TEST_CASE(PEM_PrivateRead) {
+        auto both = ARsa::generate();
+        auto priv = ARsa::fromPrivateKeyPEM(both->getPrivateKeyPEM());
+
+        auto someData = ARandom().nextBytes(10000);
+        auto result = priv->decrypt(both->encrypt(someData));
+
+        BOOST_CHECK_EQUAL(someData, result);
+
+    }
+
+    BOOST_AUTO_TEST_CASE(PEM_PublicRead) {
+        auto both = ARsa::generate();
+        auto pub = ARsa::fromPublicKeyPEM(both->getPublicKeyPEM());
+
+        auto someData = ARandom().nextBytes(10000);
+        auto result = both->decrypt(pub->encrypt(someData));
+
+        BOOST_CHECK_EQUAL(someData, result);
+
+    }
+
+BOOST_AUTO_TEST_SUITE_END()
