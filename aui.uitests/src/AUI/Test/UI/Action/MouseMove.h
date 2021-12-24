ff990e62249f604da422a308702df8d096536d14
Alex2772 <alex2772sc@gmail.com>
2021-12-24 21:09:07 +0300

    minor improvements

---
+++ aui.uitests/src/AUI/Test/UI/Action/MouseMove.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <AUI/Platform/AWindow.h>
+
+struct ViewActionMouseMove {
+    std::optional<glm::ivec2> position;
+    ViewActionMouseMove() = default;
+
+    ViewActionMouseMove(const glm::ivec2& position) : position(position) {}
+
+    void operator()(const _<AView>& view) {
+        auto coords = view->getPositionInWindow() + (position ? *position : view->getSize() / 2);
+        auto window = view->getWindow();
+        window->onMouseMove(coords);
+        UITest::frame();
+    }
+};
+
+using mouseMove = ViewActionMouseMove;
