9a3b57667a20bb1d2645b903cc96044be50b9458
Alex2772 <alex2772sc@gmail.com>
2021-12-24 22:31:50 +0300

    x

--- aui.uitests/src/AUI/Test/UI/UITestCase.cpp
+++ aui.uitests/src/AUI/Test/UI/UITestCase.cpp
@@ -13,6 +13,7 @@ using namespace boost::unit_test;
 
 APath saveScreenshot(const AString& testFilePath, const AString& name) {
     auto image = AWindow::current()->getRenderingContext()->makeScreenshot();
+    if (image.getData().empty()) return {};
     auto p = APath("reports")[APath(testFilePath).filenameWithoutExtension()];
     p.makeDirs();
     p = p[name];
@@ -25,31 +26,38 @@ void UITestCaseScope::test_unit_aborted(const test_unit& unit) {
     test_observer::test_unit_aborted(unit);
 
     // draw red rects to highlight views
-    for (auto& v : Matcher::current()->toSet()) {
-        Render::drawRectBorder(ASolidBrush{ 0xaae00000_argb },
-                               v->getPositionInWindow() - glm::ivec2{ 1, 1 },
-                               v->getSize() + glm::ivec2{ 2, 2 });
+    if (auto matcher = Matcher::current()) {
+        for (auto& v: matcher->toSet()) {
+            Render::drawRectBorder(ASolidBrush{0xaae00000_argb},
+                                   v->getPositionInWindow() - glm::ivec2{1, 1},
+                                   v->getSize() + glm::ivec2{2, 2});
+        }
     }
 
     auto name = std::string(unit.p_file_name.begin(),  unit.p_file_name.end());
     auto p = saveScreenshot(name, "abort-{}_{}.png"_format(unit.p_name->c_str(), unit.p_line_num));
 
-    std::cout << name
-              << '(' << unit.p_line_num << "): report saved at "
-              << p.absolute().systemSlashDirection().toStdString()
-              << std::endl;
+    if (!p.empty()) {
+        std::cout << name
+                  << '(' << unit.p_line_num << "): report saved at "
+                  << p.absolute().systemSlashDirection().toStdString()
+                  << std::endl;
+    }
 }
 
 void UITestCaseScope::test_unit_finish(const test_unit& unit, unsigned long i) {
+    UITest::frame();
     test_observer::test_unit_finish(unit, i);
 
     auto name = std::string(unit.p_file_name.begin(),  unit.p_file_name.end());
     auto p = saveScreenshot(name, "finish-{}_{}.png"_format(unit.p_name->c_str(), unit.p_line_num));
 
-    std::cout << name
-              << '(' << unit.p_line_num << "): screenshot saved at "
-              << p.absolute().systemSlashDirection().toStdString()
-              << std::endl;
+    if (!p.empty()) {
+        std::cout << name
+                  << '(' << unit.p_line_num << "): screenshot saved at "
+                  << p.absolute().systemSlashDirection().toStdString()
+                  << std::endl;
+    }
 
     delete this;
 }
