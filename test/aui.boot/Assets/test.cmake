60f95953b4ac0f3f76e9333d4bd1a9fe847240c8
Alex2772 <alex2772sc@gmail.com>
2022-01-17 06:37:26 +0300

    added assets test

---
+++ test/aui.boot/Assets/test.cmake
@@ -0,0 +1,14 @@
+cmake_minimum_required(VERSION 3.10)
+
+# This test imports aui::core through aui.boot, configures, compiles the project and runs the test program.
+
+if (NOT AUI_TEST_BOOT_BRANCH)
+    message(FATAL_ERROR "AUI_TEST_BOOT_BRANCH is not set")
+endif()
+
+include(../helper.cmake)
+
+find_program(_test_project test_project PATHS b/bin b/bin/Debug REQUIRED)
+
+execute_process(COMMAND ${_test_project}
+        COMMAND_ERROR_IS_FATAL ANY)
