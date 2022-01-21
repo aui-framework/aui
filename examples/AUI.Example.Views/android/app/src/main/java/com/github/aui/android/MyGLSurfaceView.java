161c937312e36776c6e2fd1dbab98171bc369cbf
alex2772 <alex2772sc@gmail.com>
2022-01-20 21:38:42 -0800

    updated android example

--- examples/AUI.Example.Views/android/app/src/main/java/com/github/aui/android/MyGLSurfaceView.java
+++ examples/AUI.Example.Views/android/app/src/main/java/com/github/aui/android/MyGLSurfaceView.java
@@ -2,45 +2,111 @@ package com.github.aui.android;
 
 import android.content.Context;
 import android.opengl.GLSurfaceView;
+import android.util.Log;
+import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
 
-import static android.view.MotionEvent.ACTION_DOWN;
-import static android.view.MotionEvent.ACTION_UP;
+import static android.view.MotionEvent.*;
+
+import java.util.NoSuchElementException;
+import java.util.Queue;
+import java.util.concurrent.LinkedBlockingDeque;
+
+import javax.microedition.khronos.opengles.GL10;
 
 public class MyGLSurfaceView extends GLSurfaceView implements View.OnTouchListener {
-    private static MyGLSurfaceView mLastSurface;
+    private static MyGLSurfaceView ourLastSurface;
+    private GestureDetector mGestureDetector;
 
     private final MyGLRenderer mRenderer;
+    private static Queue<Runnable> ourOnUiThread = new LinkedBlockingDeque<Runnable>();
 
     public MyGLSurfaceView(Context context) {
         super(context);
-        mLastSurface = this;
-        setEGLContextClientVersion(3);
+        mGestureDetector = new GestureDetector(context, new GestureDetector.OnGestureListener() {
+            @Override
+            public boolean onDown(MotionEvent motionEvent) {
+                return false;
+            }
+
+            @Override
+            public void onShowPress(MotionEvent motionEvent) {
+
+            }
+
+            @Override
+            public boolean onSingleTapUp(MotionEvent motionEvent) {
+                return false;
+            }
+
+            @Override
+            public boolean onScroll(MotionEvent motionEvent, MotionEvent motionEvent1, float velX, float velY) {
+                onUiThread(() -> handleScroll((int)motionEvent.getX(), (int)motionEvent.getY(), velX, velY));
+                return true;
+            }
+
+            @Override
+            public void onLongPress(MotionEvent motionEvent) {
+
+            }
+
+            @Override
+            public boolean onFling(MotionEvent start, MotionEvent current, float velX, float velY) {
+                Log.i("test", String.format("onFling %f, %f", velX, velY));
+                return false;
+            }
+        });
+        ourLastSurface = this;
+        setEGLContextClientVersion(2);
         setEGLConfigChooser(8, 8, 8, 8, 16, 8);
-        setRenderer(mRenderer = new MyGLRenderer());
+        setRenderer(mRenderer = new MyGLRenderer() {
+            @Override
+            public void onDrawFrame(GL10 gl) {
+                try {
+                    for (Runnable i = null; (i = ourOnUiThread.remove()) != null; ) {
+                        i.run();
+                    }
+                } catch (NoSuchElementException ignored) {}
+                super.onDrawFrame(gl);
+            }
+        });
         setRenderMode(RENDERMODE_WHEN_DIRTY);
         setOnTouchListener(this);
     }
 
     private static void requestRedraw() {
-        mLastSurface.requestRender();
+        ourLastSurface.requestRender();
     }
 
+    public static void onUiThread(Runnable runnable) {
+        ourOnUiThread.add(runnable);
+        ourLastSurface.requestRender();
+    }
 
     private static native void handleMouseButtonDown(int x, int y);
+
     private static native void handleMouseButtonUp(int x, int y);
+    private static native void handleMouseMove(int x, int y);
+    private static native void handleScroll(int originX, int originY, float velX, float velY);
 
     @Override
     public boolean onTouch(View v, MotionEvent event) {
+        int x = (int) event.getX();
+        int y = (int) event.getY();
         switch (event.getAction()) {
             case ACTION_DOWN:
-                handleMouseButtonDown((int) event.getX(), (int) event.getY());
+                onUiThread(() -> handleMouseButtonDown(x, y));
+                break;
+            case ACTION_MOVE:
+                onUiThread(() -> handleMouseMove(x, y));
                 break;
             case ACTION_UP:
-                handleMouseButtonUp((int) event.getX(), (int) event.getY());
+                onUiThread(() -> handleMouseButtonUp(x, y));
                 break;
         }
+        mGestureDetector.onTouchEvent(event);
         return true;
     }
+
 }
--- examples/AUI.Example.Views/src/ExampleWindow.cpp
+++ examples/AUI.Example.Views/src/ExampleWindow.cpp
@@ -58,6 +58,7 @@
 #include <AUI/View/AGridSplitter.h>
 #include <AUI/View/AText.h>
 #include <AUI/View/ADrawableView.h>
+#include <AUI/Traits/platform.h>
 
 using namespace ass;
 
@@ -101,7 +102,7 @@ ExampleWindow::ExampleWindow(): AWindow("Examples", 800_dp, 700_dp)
     });
     _<ATabView> tabView;
     addView(tabView = _new<ATabView>() let {
-        it->addTab(Horizontal {
+        it->addTab(AScrollArea::Builder().withContents(std::conditional_t<aui::platform::current::is_mobile(), Vertical, Horizontal> {
             Vertical {
                 // buttons
                 _new<ALabel>("Buttons"),
@@ -284,7 +285,7 @@ ExampleWindow::ExampleWindow(): AWindow("Examples", 800_dp, 700_dp)
                                 "COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR "
                                 "OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.) ") let { it->setExpanding(); },
             }
-        }, "Common");
+        }), "Common");
 
         it->addTab(Vertical {
             _new<ALabel>("Horizontal splitter"),
