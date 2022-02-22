ead1a4bcb577f787767f41ef857e64a3ebdb9c25
alexeytitov <alex2772sc@gmail.com>
2022-02-22 03:44:13 +0300

    inline fix operator<<

--- aui.core/src/AUI/Reflect/AEnumerate.h
+++ aui.core/src/AUI/Reflect/AEnumerate.h
@@ -113,4 +113,4 @@ const AMap<enum_t, AString>& AEnumerate<enum_t>::names() {
 struct AEnumerateAllValues<enum_t>{        \
     static inline constexpr AEnumerate<enum_t>::Values<__VA_ARGS__> get() {return {}; } \
 };                                         \
-std::ostream& operator<<(std::ostream& o, enum_t v) { return o << AEnumerate<enum_t>::names()[v]; }
+inline std::ostream& operator<<(std::ostream& o, enum_t v) { return o << AEnumerate<enum_t>::names()[v]; }
