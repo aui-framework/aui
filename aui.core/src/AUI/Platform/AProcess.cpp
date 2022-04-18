11dfd0b6d0a53881848f5c750c33c235a4fc2802
Alex2772 <alex2772sc@gmail.com>
2022-04-18 15:03:41 +0300

    hotfix AProcess.cpp

--- aui.core/src/AUI/Platform/AProcess.cpp
+++ aui.core/src/AUI/Platform/AProcess.cpp
@@ -24,6 +24,7 @@
 //
 
 #include "AProcess.h"
+#include "AUI/IO/AFileOutputStream.h"
 
 #if AUI_PLATFORM_WIN
 #include <windows.h>
@@ -382,11 +383,11 @@ _<AProcess> AProcess::findAnotherSelfInstance(const AString& yourProjectName) {
 
         public:
             RemoveHelper(APath&& path) : mPath(std::forward<APath>(path)) {
-                auto fos = _new<AFileOutputStream>(mPath);
+                AFileOutputStream fos(mPath);
 
                 auto n = std::to_string(self()->getPid());
-                fos->write(n.c_str(), n.length());
-                fos->close();
+                fos.write(n.c_str(), n.length());
+                fos.close();
             }
 
             ~RemoveHelper() {
