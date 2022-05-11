fa7add54a596f51e78487d4737016977c077f7ce
Alex2772 <alex2772sc@gmail.com>
2022-05-11 22:11:26 +0300

    fix json

--- aui.json/tests/Error.cpp
+++ aui.json/tests/Error.cpp
@@ -30,16 +30,49 @@
 
 
 
-TEST(Json, Error)
+TEST(Json, Error1)
 {
     EXPECT_THROW(AJson::fromString(R"({"kek": truz})"), AJsonParseException);
+}
+TEST(Json, Error2)
+{
     EXPECT_THROW(AJson::fromString(R"({"kek": falz})"), AJsonParseException);
+}
+TEST(Json, Error3)
+{
     EXPECT_THROW(AJson::fromString(R"({"array": [1,2,3,x]})"), AJsonParseException);
+}
+TEST(Json, Error4)
+{
     EXPECT_THROW(AJson::fromString(R"({"array": [1,2,3})"), AJsonParseException);
+}
+TEST(Json, Error5)
+{
     EXPECT_THROW(AJson::fromString(R"({"object": {"1"s:1,)"), AJsonParseException);
+}
+TEST(Json, Error6)
+{
     EXPECT_THROW(AJson::fromString(R"({"object": {"1":1,})"), AJsonParseException);
+}
+TEST(Json, Error7)
+{
     EXPECT_THROW(AJson::fromString(R"({"object": {"1":2})"), AJsonParseException);
+}
+TEST(Json, Error8)
+{
     EXPECT_THROW(AJson::fromString(R"({"object": {"1":2 xs})"), AJsonParseException);
+}
+TEST(Json, Error9)
+{
     EXPECT_THROW(AJson::fromString(R"(<?xml version='1.0' encoding='UTF-8'?>)"), AJsonParseException);
+}
+TEST(Json, Error10)
+{
+    EXPECT_NO_THROW(AJson::fromString(R"({
+  "game": {
+    "info": "http://mdownload.kara-online.ru/main/bin/info.json",
+    "manifest": "http://mdownload.kara-online.ru/main/bin/manifest.json"
+  }
+})"));
 }
 
