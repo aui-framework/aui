563d74f37fe825668632f1d3e139368b73de1c41
alex2772 <alex2772sc@gmail.com>
2022-06-23 15:17:17 +0300

    added docs about writing tests

--- docs/projects/hello_world/basic/src/main.cpp
+++ docs/projects/hello_world/basic/src/main.cpp
@@ -1,7 +1,9 @@
 #include <AUI/Platform/Entry.h>
 #include <AUI/Logging/ALogger.h>
 
+static constexpr auto LOG_TAG = "MyApp";
+
 AUI_ENTRY {
-    ALogger::info("Hello world!");
+    ALogger::info(LOG_TAG) << "Hello world!";
     return 0;
 }
