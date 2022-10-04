4f30a47d34f0e87028fcc975e20271bad3a70fd5
Alex2772 <alex2772sc@gmail.com>
2022-10-04 16:38:51 +0300

    fix image loading bug + refactor aui.image

--- aui.image/src/AUI/Image/jpg/JpgImageLoader.h
+++ aui.image/src/AUI/Image/jpg/JpgImageLoader.h
@@ -26,13 +26,12 @@
 #pragma once
 
 
-#include <AUI/Image/IImageLoader.h>
+#include <AUI/api.h>
+#include <AUI/Image/StbImageLoader.h>
 
-class JpgImageLoader: public IImageLoader {
+class JpgImageLoader: public StbImageLoader {
 public:
     bool matches(AByteBufferView buffer) override;
-
-    _<AImage> getRasterImage(AByteBufferView buffer) override;
 };
 
 
