1b4933a2c405409d20a6ecf24a2ca8b0703f36bb
maksimdervis <maximdervis@gmail.com>
2023-05-05 21:09:45 +0300

    fix pipeline

--- aui.views/src/AUI/View/AAbstractTextField.cpp
+++ aui.views/src/AUI/View/AAbstractTextField.cpp
@@ -179,7 +179,7 @@ void AAbstractTextField::invalidateFont() {
 }
 
 void AAbstractTextField::onCharEntered(wchar_t c) {
-    mCursorIndex = std::min(mCursorIndex, mContents.size());
+    mCursorIndex = std::min(mCursorIndex, static_cast<unsigned int> (mContents.size()));
     if (c == '\n' || c == '\r')
         return;
     AView::onCharEntered(c);
