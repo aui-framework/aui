3c531d6e5a50a9b90de769ef80f5a33e4fdc5c60
Максим Дервис <57065446+maximdervis@users.noreply.github.com>
2023-05-17 00:57:00 +0300

    doLogFileAccessSafe (#146)
    * doLogFileAccessSafe
    
    * docs added
    
    * added exceptions

--- aui.core/src/AUI/Logging/ALogger.h
+++ aui.core/src/AUI/Logging/ALogger.h
@@ -233,6 +233,25 @@ public:
         return global().mLogFile.path();
     }
 
+    /*
+     * @brief Allows to perform some action (access safely)
+     * on log file (which is opened all over the execution process)
+     * @details
+     * Useful when sending log file to remote server:
+     * @note Windows, for instance, doesn't allow to read the file when it's already opened
+     */
+    template <aui::invocable Callable>
+    static void doLogFileAccessSafe(Callable action) {
+        if (!global().mLogFile.nativeHandle()) {
+            action();
+            return;
+        }
+
+        global().mLogFile.close();
+        action();
+        global().mLogFile.open(true);
+    }
+
     static LogWriter info(const AString& str)
     {
         return {global(), INFO, str};
