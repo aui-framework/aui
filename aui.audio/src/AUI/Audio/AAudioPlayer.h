5b37790e9cd20011cc6d46863c700c929a30bced
ilyazavalov <ilya.zavalov@gmail.com>
2023-09-16 04:49:35 +0300

    trash removed

--- aui.audio/src/AUI/Audio/AAudioPlayer.h
+++ aui.audio/src/AUI/Audio/AAudioPlayer.h
@@ -181,20 +181,4 @@ private:
     void stopImpl();
 
     void onSourceSet();
-
-    /**
-     * @return callback that apply to audio sample the player's current volume level
-     */
-    auto getVolumeCallback() {
-        return [self = _cast<AAudioPlayer>(sharedPtr())](std::span<std::byte> rawSample) {
-            if (rawSample.size() != self->mSource->info().bitsPerSample() / 8) {
-                return;
-            }
-
-            int16_t sample = *(reinterpret_cast<int16_t*>(rawSample.data()));
-            float volume = self->volume();
-            sample = static_cast<int16_t>(volume * static_cast<float>(sample) /*+ (volume - 1) * 32768*/);
-            std::memcpy(rawSample.data(), &sample, 2);
-        };
-    }
 };
