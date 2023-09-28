5ae3ca5eae2315240be4740111318e1063f9244c
alex2772 <alex2772sc@gmail.com>
2023-09-28 16:09:46 +0300

    small fixes

--- aui.views/src/AUI/View/AScrollArea.cpp
+++ aui.views/src/AUI/View/AScrollArea.cpp
@@ -163,16 +163,23 @@ void AScrollArea::onPointerReleased(const APointerReleasedEvent& event) {
 }
 
 void AScrollArea::scrollTo(const _<AView>& target, bool nearestBorder) {
-    auto toBeginPoint = target->getPositionInWindow() - getPositionInWindow();
+    const auto targetBegin = target->getPositionInWindow();
+    const auto myBegin = getPositionInWindow();
+
+    const auto toBeginPoint = targetBegin - myBegin;
     if (!nearestBorder) {
         scroll(toBeginPoint);
         return;
     }
 
-    auto toEndPoint = toBeginPoint + getSize() - target->getSize();
-    auto delta = target->getCenterPointInWindow() - getCenterPointInWindow();
-    auto thresholdForDelta = getSize() / 2;
+    const auto targetEnd = targetBegin + target->getSize();
+    const auto myEnd = myBegin + getSize();
+
+    const auto toEndPoint = targetEnd - myEnd;
+
+    auto delta = (targetBegin + target->getSize() / 2) - (myBegin + getSize() / 2);
     auto direction = glm::greaterThan(delta, glm::ivec2(0));
-    
-    scroll(glm::mix(toBeginPoint, toEndPoint, direction) * glm::ivec2(glm::greaterThan(glm::abs(delta), thresholdForDelta)));
+    const auto toBeginPointConditional = toBeginPoint * glm::ivec2(glm::greaterThan(myBegin, targetBegin));
+    const auto toEndPointConditional = toEndPoint * glm::ivec2(glm::greaterThan(targetEnd, myEnd));
+    scroll(glm::mix(toBeginPointConditional, toEndPointConditional, direction));
 }
