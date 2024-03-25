5a7aa8e25d8c862c82d108803e6888bbfe76d2d0
alex2772 <alex2772sc@gmail.com>
2024-03-25 07:31:11 +0300

    tryfix windows + changed address in PingTest

--- aui.network/tests/PingTest.cpp
+++ aui.network/tests/PingTest.cpp
@@ -18,5 +18,5 @@
 #include "AUI/Network/AIcmp.h"
 
 TEST(Ping, Case1) {
-    EXPECT_GE(AIcmp::ping(AInet4Address("1.1.1.1"))->count(), 0);
+    EXPECT_GE(AIcmp::ping(AInet4Address("github.com"))->count(), 0);
 }
