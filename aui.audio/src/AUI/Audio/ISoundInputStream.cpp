d447901867dd1fd7f28c8f1aada3b8b9befb0f1b
ilyazavalov <ilya.zavalov@gmail.com>
2024-04-17 22:03:33 +0300

    build fix

--- aui.audio/src/AUI/Audio/ISoundInputStream.cpp
+++ aui.audio/src/AUI/Audio/ISoundInputStream.cpp
@@ -25,7 +25,7 @@ _<ISoundInputStream> ISoundInputStream::fromUrl(const AUrl& url) {
         throw;
     }
 
-    throw AException("Failed to create ISoundInputStream from url = {}: unsupported format"_format(url));
+    throw AException("Failed to create ISoundInputStream from url = {}: unsupported format"_format(url.full()));
 }
 
 ISoundInputStream::Cache& ISoundInputStream::Cache::inst() {
