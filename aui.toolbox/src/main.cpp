da89dafc4132e661f26eb7daabe45f28d099cfd9
alex2772 <alex2772sc@gmail.com>
2024-05-02 23:57:20 +0300

    added hookah

--- aui.toolbox/src/main.cpp
+++ aui.toolbox/src/main.cpp
@@ -14,6 +14,33 @@
 // You should have received a copy of the GNU Lesser General Public
 // License along with this library. If not, see <http://www.gnu.org/licenses/>.
 
+/*
+                                     ,*%%%
+                                      ,%%%
+                                (%%%%%%%%%%%%%%%
+                                    /%%#  %%
+                                      (% %
+                                      /%,&
+                                      ,% &         (%%%,.%%%
+                                      ,%,&     ,%%%        %&
+                                %% .  %%.%  %&&%           %
+                                  .# %% %  %#            *%
+                                   %%%%%%%%%           #%.
+                                  %%%%%%&(   &      %%%
+                                   ,%%%%%%%%    *%%
+                                    &#(    %#%%
+                                   % .  /##.(
+                                 %, /        %/
+                            (%%. ,*(%&&&&&% (   ##
+                       *%%/  /%% %,%%%%%%%%%%%/,%/%
+                    %#.      % %%%%%%%%%%%%%%%.%% &
+                   #           %/ &%%%%%%%%%%# %%
+                  %(            %#&% .%%%% *#/%,
+                   ,/#             *(######(*                ,
+                       (&#,%(.                 #&,%%%%%%%%%.
+
+*/
+
 #include <AUI/Platform/Entry.h>
 #include <Command/Pack.h>
 #include <Command/Lang.h>
