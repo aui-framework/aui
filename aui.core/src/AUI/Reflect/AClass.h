7df6ef3b37fd4031165f6a4ea2fbd296ebfef8e7
alex2772 <alex2772sc@gmail.com>
2024-11-19 16:50:32 +0300

    fix clang AClass

--- aui.core/src/AUI/Reflect/AClass.h
+++ aui.core/src/AUI/Reflect/AClass.h
@@ -10,57 +10,57 @@
  */
 
 #pragma once
+
 #include "AUI/Common/AString.h"
 
 
-
 template<class T>
-class AClass
-{
+class AClass {
 public:
-	static AString name()
-	{
-#if defined(_MSC_VER)
-		AString s = __FUNCSIG__;
-		auto openTag = s.find('<') + 1;
-		auto closeTag = s.find('>');
-		auto name = s.substr(openTag, closeTag - openTag);
+    static AString name() {
+#if AUI_COMPILER_MSVC
+        AString s = __FUNCSIG__;
+        auto openTag = s.find('<') + 1;
+        auto closeTag = s.find('>');
+        auto name = s.substr(openTag, closeTag - openTag);
         name = name.substr(name.rfind(' ') + 1);
-		if (name.endsWith(" &"))
-			name = name.substr(0, name.length() - 2);
-		return name;
-#elif AUI_PLATFORM_ANDROID
-		AString s = __PRETTY_FUNCTION__;
-		auto b = s.find("=") + 1;
-		auto e = s.find("&", b);
-		auto result = s.substr(b, e - b);
-		result = result.trim();
-		return result;
+        if (name.endsWith(" &"))
+            name = name.substr(0, name.length() - 2);
+        return name;
+#elif AUI_COMPILER_CLANG
+        AString s = __PRETTY_FUNCTION__;
+        auto b = s.find("=") + 1;
+        auto e = s.find("&", b);
+        e = std::min(s.find("]", b), e);
+        auto result = s.substr(b, e - b);
+        result = result.trim();
+        return result;
 #else
-		AString s = __PRETTY_FUNCTION__;
-		auto b = s.find("with T = ") + 9;
+        AString s = __PRETTY_FUNCTION__;
+        auto b = s.find("with T = ") + 9;
         return { s.begin() + b, s.end() - 1 };
 #endif
-	}
+    }
 
-	static AString nameWithoutNamespace() {
+    static AString nameWithoutNamespace() {
         auto s = name();
         auto p = s.rfind("::");
         if (p != AString::NPOS) {
             return {s.begin() + p + 2, s.end()};
         }
         return s;
-	}
+    }
 
-	static AString toString(const T& t) {
+    static AString toString(const T& t) {
         return "<object of type " + name() + ">";
-	}
+    }
 };
 
 template<>
 inline AString AClass<AString>::toString(const AString& t) {
     return "\"" + t + "\"";
 }
+
 template<>
 inline AString AClass<int>::toString(const int& t) {
     return AString::number(t);
