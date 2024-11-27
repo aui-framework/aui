9f763ef1643dbf09f54e181431486a7b835b97a5
alex2772 <alex2772sc@gmail.com>
2024-11-27 04:01:25 +0300

    u

---
+++ aui.views/src/AUI/View/AViewContainer.h
@@ -0,0 +1,41 @@
+/*
+ * AUI Framework - Declarative UI toolkit for modern C++20
+ * Copyright (C) 2020-2024 Alex2772 and Contributors
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#pragma once
+
+#include "AViewContainerBase.h"
+
+/**
+ * @brief A trivial modifiable view that represents a set of views.
+ * @ingroup useful_views
+ * @details
+ * AViewContainer can store, render, resize, provide events to and handle the child views.
+ *
+ * AViewContainer does not control the position and size of the child views by itself; instead, it delegates that
+ * responsibility to it's @ref layout_managers "layout manager".
+ *
+ * Since AViewContainer is an instance of AView, AViewContainer can handle AViewContainers recursively, thus, making
+ * possible complex UI by nested AViewContainers with different layout managers.
+ */
+class API_AUI_VIEWS AViewContainer final: public AViewContainerBase {
+public:
+    AViewContainer();
+    ~AViewContainer() override;
+
+    using AViewContainerBase::addView;
+    using AViewContainerBase::addViews;
+    using AViewContainerBase::removeView;
+    using AViewContainerBase::removeAllViews;
+    using AViewContainerBase::setViews;
+    using AViewContainerBase::setContents;
+    using AViewContainerBase::getLayout;
+    using AViewContainerBase::setLayout;
+};
