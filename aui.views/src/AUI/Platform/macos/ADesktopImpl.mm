e594b06bb35b17bbedf59079b37dbe5958076d52
Axel Nana <axel.nana@aliens-group.com>
2024-12-11 10:23:14 +0100

    macos file picker dialog.

--- aui.views/src/AUI/Platform/macos/ADesktopImpl.mm
+++ aui.views/src/AUI/Platform/macos/ADesktopImpl.mm
@@ -12,6 +12,8 @@
 #include <AUI/Platform/ADesktop.h>
 #include <AUI/Util/ARaiiHelper.h>
 
+#include <AUI/Platform/AWindow.h>
+
 #include <AppKit/AppKit.h>
 #include <Availability.h>
 
@@ -40,12 +42,119 @@ glm::ivec2 ADesktop::getMousePosition() {
 void ADesktop::setMousePos(const glm::ivec2& pos) {}
 
 AFuture<APath> ADesktop::browseForDir(AWindowBase* parent, const APath& startingLocation) {
-    return async { return APath(); };
+    AUI_NULLSAFE(parent)->blockUserInput();
+    AUI_DEFER { AUI_NULLSAFE(parent)->blockUserInput(false); };
+
+    @autoreleasepool {
+        NSOpenPanel* panel = [NSOpenPanel openPanel];
+        [panel setCanChooseFiles:NO];
+        [panel setCanChooseDirectories:YES];
+        [panel setAllowsMultipleSelection:NO];
+        [panel setCanCreateDirectories:YES];
+
+        if (startingLocation.exists()) {
+            NSString* path = [NSString stringWithUTF8String:startingLocation.toStdString().c_str()];
+            [panel setDirectoryURL:[NSURL fileURLWithPath:path]];
+        }
+
+        NSWindow* nativeParentWindow = nil;
+        if (auto d = dynamic_cast<AWindow*>(parent)) {
+            nativeParentWindow = static_cast<NSWindow*>(d->nativeHandle());
+        }
+
+        __block NSModalResponse response = NSModalResponseCancel;
+
+        if (nativeParentWindow) {
+            [panel
+                beginSheetModalForWindow:nativeParentWindow
+                       completionHandler:^(NSModalResponse result) {
+                         response = result;
+                       }];
+        } else {
+            response = [panel runModal];
+        }
+
+        if (response == NSModalResponseOK) {
+            NSURL* selectedFolder = [[panel URLs] objectAtIndex:0];
+            return AFuture(APath([[selectedFolder path] UTF8String]));
+        }
+    }
+
+    return AFuture(APath());
 }
 
 AFuture<APath>
 ADesktop::browseForFile(AWindowBase* parent, const APath& startingLocation, const AVector<FileExtension>& extensions) {
-    return async { return APath(); };
+    AUI_NULLSAFE(parent)->blockUserInput();
+    AUI_DEFER { AUI_NULLSAFE(parent)->blockUserInput(false); };
+
+    @autoreleasepool {
+        NSOpenPanel* panel = [NSOpenPanel openPanel];
+        [panel setCanChooseFiles:YES];
+        [panel setCanChooseDirectories:NO];
+        [panel setAllowsMultipleSelection:NO];
+        [panel setCanCreateDirectories:NO];
+
+        if (startingLocation.exists()) {
+            NSString* path = [NSString stringWithUTF8String:startingLocation.toStdString().c_str()];
+            [panel setDirectoryURL:[NSURL fileURLWithPath:path]];
+        }
+
+        if (!extensions.empty()) {
+#if AUI_MACOS_SUPPORTS_CONTENTTYPES == 1
+            NSMutableArray<UTType*>* allowedTypes = [NSMutableArray array];
+            for (const auto& ext : extensions) {
+                if (ext.extension == "*") {
+                    [allowedTypes removeAllObjects];
+                    break;
+                }
+
+                UTType* type = [UTType
+                    typeWithFilenameExtension:[NSString stringWithUTF8String:ext.extension.toStdString().c_str()]];
+
+                [allowedTypes addObject:type];
+            }
+
+            [panel setAllowedContentTypes:allowedTypes];
+#else
+            NSMutableArray<NSString*>* allowedExtensions = [NSMutableArray array];
+            for (const auto& ext : extensions) {
+                if (ext.extension == "*") {
+                    [allowedExtensions removeAllObjects];
+                    break;
+                }
+
+                [allowedExtensions addObject:[NSString stringWithUTF8String:ext.extension.toStdString().c_str()]];
+            }
+
+            [panel setAllowedFileTypes:allowedExtensions];
+#endif
+        }
+
+        NSWindow* nativeParentWindow = nil;
+        if (auto d = dynamic_cast<AWindow*>(parent)) {
+            nativeParentWindow = static_cast<NSWindow*>(d->nativeHandle());
+        }
+
+        __block NSModalResponse response = NSModalResponseCancel;
+
+        if (nativeParentWindow) {
+            [panel
+                beginSheetModalForWindow:nativeParentWindow
+                       completionHandler:^(NSModalResponse result) {
+                         response = result;
+                       }];
+        } else {
+            response = [panel runModal];
+        }
+
+        if (response == NSModalResponseOK) {
+            NSURL* selectedFolder = [[panel URLs] objectAtIndex:0];
+            return AFuture(APath([[selectedFolder path] UTF8String]));
+        }
+    }
+
+    return AFuture(APath());
 }
 
 _<IDrawable> ADesktop::iconOfFile(const APath& file) { return nullptr; }
