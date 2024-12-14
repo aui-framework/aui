5a5a9c702063bf63df38730cd5a0c6752171c730
Axel Nana <axel.nana@aliens-group.com>
2024-12-14 22:11:40 +0100

    linux message box.

--- aui.views/src/AUI/Platform/linux/AMessageBoxImpl.cpp
+++ aui.views/src/AUI/Platform/linux/AMessageBoxImpl.cpp
@@ -9,6 +9,8 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
+#include <libnotify/notify.h>
+
 #include "AUI/Common/AException.h"
 #include "AUI/Platform/AMessageBox.h"
 #include "AUI/Platform/AWindow.h"
@@ -18,73 +20,93 @@
 #include "AUI/Thread/AEventLoop.h"
 #include "AUI/Util/ARaiiHelper.h"
 
+struct AMessageBoxContext {
+    AMessageBox::ResultButton result;
+    GMainLoop *loop;
+};
 
-namespace {
-    class MessageBox: public AWindow {
-    public:
-        MessageBox(AWindow *parent, const AString &title, const AString &message, AMessageBox::Icon icon,
-                   AMessageBox::Button b): AWindow(title, 300_dp, 200_dp, parent, WindowStyle::MODAL | WindowStyle::NO_MINIMIZE_MAXIMIZE | WindowStyle::NO_RESIZE) {
-            using namespace declarative;
-            setContents(Vertical {
-                AText::fromString(message),
-                Centered {
-                    [&]() -> _<AView> {
-                        switch (b) {
-                            case AMessageBox::Button::OK: return Button { "OK"_i18n }.clicked(me::onOk);
-                            case AMessageBox::Button::OK_CANCEL: return Horizontal {
-                                Button { "OK"_i18n }.clicked(me::onOk),
-                                Button { "Cancel"_i18n }.clicked(me::onCancel),
-                            };
-                            case AMessageBox::Button::YES_NO: return Horizontal {
-                                Button { "Yes"_i18n }.clicked(me::onYes),
-                                Button { "No"_i18n }.clicked(me::onNo),
-                            };
-                            case AMessageBox::Button::YES_NO_CANCEL: return Horizontal {
-                                Button { "Yes"_i18n }.clicked(me::onYes),
-                                Button { "No"_i18n }.clicked(me::onNo),
-                                Button { "Cancel"_i18n }.clicked(me::onCancel),
-                            };
-                            default: throw AException("invalid AMessageBox::Button");
-                        }
-                    },
-                }
-            });
-        }
-    signals:
-        emits<AMessageBox::ResultButton /* result */> status;
-
-    private:
-        void onOk() {
-            emit status(AMessageBox::ResultButton::OK);
-        }
-        void onCancel() {
-            emit status(AMessageBox::ResultButton::CANCEL);
-        }
-        void onYes() {
-            emit status(AMessageBox::ResultButton::YES);
-        }
-        void onNo() {
-            emit status(AMessageBox::ResultButton::NO);
-        }
-    };
+static void on_notification_closed(NotifyNotification *notification, gpointer user_data) {
+    const auto *data = static_cast<AMessageBoxContext *>(user_data);
+    g_main_loop_quit(data->loop);
 }
 
-AMessageBox::ResultButton
-AMessageBox::show(AWindow *parent, const AString &title, const AString &message, AMessageBox::Icon icon,
-                  AMessageBox::Button b) {
+static void on_action(NotifyNotification *notification, char *action, gpointer user_data) {
+    auto *data = static_cast<AMessageBoxContext *>(user_data);
 
-    auto a = _new<MessageBox>(parent, title, message, icon, b);
-    auto& loop = AWindow::getWindowManager();
-    AMessageBox::ResultButton result = AMessageBox::ResultButton::INVALID;
-    AObject::connect(a->status, a, [&](AMessageBox::ResultButton r) {
-        a->close();
-        loop.stop();
-        result = r;
-    });
-    a->show();
+    if (strcmp(action, "yes") == 0) {
+        data->result = AMessageBox::ResultButton::YES;
+    } else if (strcmp(action, "no") == 0) {
+        data->result = AMessageBox::ResultButton::NO;
+    } else if (strcmp(action, "ok") == 0) {
+        data->result = AMessageBox::ResultButton::OK;
+    } else if (strcmp(action, "cancel") == 0) {
+        data->result = AMessageBox::ResultButton::CANCEL;
+    } else {
+        data->result = AMessageBox::ResultButton::INVALID;
+    }
 
-    ARaiiHelper loopStarter = [&] { loop.start(); };
-    loop.loop();
+    g_main_loop_quit(data->loop);
+}
 
-    return result;
+AMessageBox::ResultButton AMessageBox::show(
+    AWindow *parent, const AString &title, const AString &message, AMessageBox::Icon icon, AMessageBox::Button b) {
+    if (!notify_init("AUI_App")) {
+        ALogger::err("AUI") << "Unable to initialize libnotify";
+        return AMessageBox::ResultButton::INVALID;
+    }
+
+    NotifyNotification *notification =
+        notify_notification_new(title.toStdString().c_str(), message.toStdString().c_str(), [icon] {
+            switch (icon) {
+                case Icon::INFO:
+                    return "dialog-information";
+                case Icon::WARNING:
+                    return "dialog-warning";
+                case Icon::CRITICAL:
+                    return "dialog-error";
+                default:
+                    return "dialog-question";
+            }
+        }());
+
+    AMessageBoxContext c {};
+    c.result = ResultButton::INVALID;
+    c.loop = g_main_loop_new(nullptr, FALSE);
+
+    notify_notification_set_urgency(notification, NOTIFY_URGENCY_CRITICAL);
+
+    if (b == Button::YES_NO_CANCEL || b == Button::YES_NO) {
+        notify_notification_add_action(notification, "yes", "Yes"_i18n.toStdString().c_str(), on_action, &c, nullptr);
+
+        notify_notification_add_action(notification, "no", "No"_i18n.toStdString().c_str(), on_action, &c, nullptr);
+
+        if (b == Button::YES_NO_CANCEL) {
+            notify_notification_add_action(
+                notification, "cancel", "Cancel"_i18n.toStdString().c_str(), on_action, &c, nullptr);
+        }
+    } else if (b == Button::OK_CANCEL || b == Button::OK) {
+        notify_notification_add_action(notification, "ok", "OK"_i18n.toStdString().c_str(), on_action, &c, nullptr);
+
+        if (b == Button::OK_CANCEL) {
+            notify_notification_add_action(
+                notification, "cancel", "Cancel"_i18n.toStdString().c_str(), on_action, &c, nullptr);
+        }
+    }
+
+    g_signal_connect(notification, "closed", G_CALLBACK(on_notification_closed), &c);
+
+    if (!notify_notification_show(notification, nullptr)) {
+        std::cerr << "Failed to show notification" << std::endl;
+        g_object_unref(notification);
+        notify_uninit();
+        return ResultButton::INVALID;
+    }
+
+    g_main_loop_run(c.loop);
+
+    g_object_unref(notification);
+    g_main_loop_unref(c.loop);
+    notify_uninit();
+
+    return c.result;
 }
