dbaf25b7d6c0a3fe5820875924e1e45105e88792
Alex2772 <alex2772sc@gmail.com>
2024-12-16 15:13:08 +0300

    Feat/wholearchive (#393)
    * tryfix wholearchive stuff
    
    * added gtk reference (TO REVERT)
    
    * Revert "added gtk reference (TO REVERT)"
    
    This reverts commit 063f27d4272a202659647c82beedbac01b557e49.
    
    * tryfix wholearchive
    
    * added views test; added distinguish between static and shared libs in aui-config.cmake
    
    * tryfix backtrace
    
    * added system library diagnostics + tryfix ctest

--- aui.core/cmake/backtrace.cmake
+++ aui.core/cmake/backtrace.cmake
@@ -21,6 +21,6 @@ set_target_properties(backtrace PROPERTIES
                       INTERFACE_INCLUDE_DIRECTORIES ${CMAKE_INSTALL_PREFIX}/include)
 ]])
 
-configure_file(backtrace-config.cmake.in ${CMAKE_BINARY_DIR}/backtrace-config.cmake @only)
+configure_file(backtrace-config.cmake.in ${CMAKE_BINARY_DIR}/backtrace-config.cmake)
 
 install(FILES ${CMAKE_BINARY_DIR}/backtrace-config.cmake DESTINATION .)
---
+++ test/aui.boot/ViewsProgram/test_project/src/main.cpp
@@ -0,0 +1,22 @@
+/*
+ * AUI Framework - Declarative UI toolkit for modern C++20
+ * Copyright (C) 2020-2024 Alex2772 and Contributors
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <AUI/Platform/Entry.h>
+#include <AUI/Platform/AWindow.h>
+#include <AUI/Logging/ALogger.h>
+
+AUI_ENTRY {
+    ALogger::info("Hello world!");
+    if (args.contains("--real-run")) {
+        _new<AWindow>()->show();
+    }
+    return 0;
+}
