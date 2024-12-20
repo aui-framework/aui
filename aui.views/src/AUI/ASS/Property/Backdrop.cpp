1c5e84d655d7e3130157e784ca7cd2d4aeb6a845
alex2772 <alex2772sc@gmail.com>
2024-12-20 14:27:08 +0300

    tryfix clang

--- aui.views/src/AUI/ASS/Property/Backdrop.cpp
+++ aui.views/src/AUI/ASS/Property/Backdrop.cpp
@@ -20,7 +20,7 @@
 #include "IProperty.h"
 
 void ass::prop::Property<ass::Backdrop>::renderFor(AView* view, const ARenderContext& ctx) {
-    ctx.render.backdrops({}, view->getSize(), mInfo.effects);
+    ctx.render.backdrops({}, view->getSize(), std::span<ass::Backdrop::Any>(mInfo.effects.data(), mInfo.effects.size()));
 }
 
 bool ass::prop::Property<ass::Backdrop>::isNone() { return mInfo.effects.empty(); }
