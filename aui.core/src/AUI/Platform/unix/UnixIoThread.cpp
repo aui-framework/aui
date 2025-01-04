b0a5855b8ff13d9b652250030de82434f6e35723
alex2772 <alex2772sc@gmail.com>
2025-01-04 22:53:22 +0300

    fix tests?

--- aui.core/src/AUI/Platform/unix/UnixIoThread.cpp
+++ aui.core/src/AUI/Platform/unix/UnixIoThread.cpp
@@ -33,12 +33,12 @@ UnixIoThread& UnixIoThread::inst() noexcept {
 #include <AUI/Platform/ErrorToException.h>
 
 
-class MyEventLoop: public IEventLoop {
+class UnixIoEventLoop: public IEventLoop {
 public:
-    MyEventLoop(UnixIoThread& parent) : mParent(parent) {
+    UnixIoEventLoop(UnixIoThread& parent) : mParent(parent) {
     }
 
-    ~MyEventLoop() override {
+    ~UnixIoEventLoop() override {
 
     }
 
@@ -100,7 +100,7 @@ void UnixIoThread::unregisterCallback(int fd) noexcept {
 
 UnixIoThread::UnixIoThread() noexcept: mThread(_new<AThread>([&] {
     AThread::setName("AUI IO");
-    MyEventLoop loop(*this);
+    UnixIoEventLoop loop(*this);
     IEventLoop::Handle handle(&loop);
     AThread::current()->getCurrentEventLoop()->loop();
 })), mEpollFd(epoll_create1(0)) {
@@ -118,11 +118,11 @@ UnixIoThread::UnixIoThread() noexcept: mThread(_new<AThread>([&] {
 
 #else
 
-class MyEventLoop: public IEventLoop {
+class UnixIoEventLoop: public IEventLoop {
 public:
-    MyEventLoop(UnixIoThread& parent) : mParent(parent) {}
+    UnixIoEventLoop(UnixIoThread& parent) : mParent(parent) {}
 
-    ~MyEventLoop() override {
+    ~UnixIoEventLoop() override {
 
     }
 
@@ -189,7 +189,7 @@ void UnixIoThread::unregisterCallback(int fd) noexcept {
 
 UnixIoThread::UnixIoThread() noexcept: mThread(_new<AThread>([&] {
     AThread::setName("AUI IO");
-    MyEventLoop loop(*this);
+    UnixIoEventLoop loop(*this);
     IEventLoop::Handle handle(&loop);
     AThread::current()->getCurrentEventLoop()->loop();
 })) {
