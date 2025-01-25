90e886e1ec90c4f0686aa49f4fb3748334a7382a
Alex2772 <alex2772sc@gmail.com>
2025-01-25 23:55:37 +0300

    readZip

--- aui.core/src/AUI/Util/LZ.cpp
+++ aui.core/src/AUI/Util/LZ.cpp
@@ -11,19 +11,22 @@
 
 #include "LZ.h"
 
+#include "kAUI.h"
+
 #include <stdexcept>
 #include <string>
 #include "AUI/Common/AByteBuffer.h"
 #include "AUI/Traits/memory.h"
 
 #include <zlib.h>
-
+#include <minizip/unzip.h>
 
 void aui::zlib::compress(AByteBufferView b, AByteBuffer& dst) {
     uLong len = b.size() * 3 / 2 + 0xff;
     dst.reserve(dst.getSize() + len);
-    int r = compress2(reinterpret_cast<Bytef*>(const_cast<char*>(dst.end())), &len,
-                      reinterpret_cast<Bytef*>(const_cast<char*>(b.data())), b.size(), Z_BEST_COMPRESSION);
+    int r = compress2(
+        reinterpret_cast<Bytef*>(const_cast<char*>(dst.end())), &len,
+        reinterpret_cast<Bytef*>(const_cast<char*>(b.data())), b.size(), Z_BEST_COMPRESSION);
     if (r != Z_OK) {
         throw std::runtime_error(std::string("zlib compress error ") + std::to_string(r));
     }
@@ -34,8 +37,8 @@ void aui::zlib::decompress(AByteBufferView b, AByteBuffer& dst) {
     for (size_t i = 4;; i++) {
         dst.reserve(b.size() * i);
         uLong len = dst.endReserved() - dst.end();
-        int r = uncompress(reinterpret_cast<Bytef*>(dst.end()), &len,
-                           reinterpret_cast<Bytef*>(const_cast<char*>(b.data())), b.size());
+        int r = uncompress(
+            reinterpret_cast<Bytef*>(dst.end()), &len, reinterpret_cast<Bytef*>(const_cast<char*>(b.data())), b.size());
         switch (r) {
             case Z_BUF_ERROR:
                 continue;
@@ -49,11 +52,11 @@ void aui::zlib::decompress(AByteBufferView b, AByteBuffer& dst) {
 }
 
 _unique<IInputStream> aui::zlib::decompressToStream(AByteBufferView b) {
-    class LZByteBufferViewDecompressStream: public IInputStream, public aui::noncopyable {
+    class LZByteBufferViewDecompressStream : public IInputStream, public aui::noncopyable {
     public:
         LZByteBufferViewDecompressStream(AByteBufferView b) {
             aui::zero(mStream);
-            mStream.next_in = (Bytef*)b.data();
+            mStream.next_in = (Bytef*) b.data();
             mStream.avail_out = 0;
             if (auto r = inflateInit(&mStream); r != Z_OK) {
                 throw AZLibException("zlib decompress error " + AString::number(r));
@@ -63,7 +66,7 @@ _unique<IInputStream> aui::zlib::decompressToStream(AByteBufferView b) {
 
         size_t read(char* dst, size_t size) override {
             auto prevTotalOut = mStream.total_out;
-            mStream.next_out = (Bytef*)dst;
+            mStream.next_out = (Bytef*) dst;
             mStream.avail_out = size;
             auto r = inflate(&mStream, Z_NO_FLUSH);
             if (r != Z_STREAM_END && r != Z_OK) {
@@ -72,14 +75,92 @@ _unique<IInputStream> aui::zlib::decompressToStream(AByteBufferView b) {
             return mStream.total_out - prevTotalOut;
         }
 
-        ~LZByteBufferViewDecompressStream() override {
-            inflateEnd(&mStream);
-        }
+        ~LZByteBufferViewDecompressStream() override { inflateEnd(&mStream); }
 
     private:
         z_stream mStream;
-
     };
 
     return std::make_unique<LZByteBufferViewDecompressStream>(b);
+}
+
+size_t aui::zlib::ZipEntry::Stream::read(char* dst, size_t size) {
+    return unzReadCurrentFile(mHandle, dst, size);
+}
+
+aui::zlib::ZipEntry::Stream::~Stream() {
+    if (mHandle) {
+        unzCloseCurrentFile(mHandle);
+    }
+}
+
+aui::zlib::ZipEntry::Stream aui::zlib::ZipEntry::open(const std::string& password) const {
+    if (auto err = unzOpenCurrentFile3(mHandle, nullptr, nullptr, false, password.empty() ? nullptr : password.c_str()); err != UNZ_OK) {
+        throw AZLibException("unzOpenCurrentFile3 failed: {}"_format(err));
+    }
+    return Stream(mHandle);
+}
+
+void aui::zlib::readZip(aui::no_escape<ISeekableInputStream> stream, const std::function<void(const ZipEntry&)>& visitor) {
+    zlib_filefunc_def funcs = {
+        .zopen_file = [](voidpf opaque, const char* filename, int mode) -> voidpf { return opaque; },
+        .zread_file = [](voidpf opaque, voidpf stream, void* buf, uLong size) -> uLong {
+            return static_cast<ISeekableInputStream*>(opaque)->read(static_cast<char*>(buf), size);
+        },
+        .zwrite_file = [](voidpf opaque, voidpf stream, const void* buf, uLong size) -> uLong { return 0; },
+        .ztell_file = [](voidpf opaque, voidpf stream) -> long {
+            return static_cast<ISeekableInputStream*>(opaque)->tell();
+        },
+        .zseek_file = [](voidpf opaque, voidpf stream, uLong offset, int origin) -> long {
+            static_cast<ISeekableInputStream*>(opaque)->seek(offset, [&] {
+                switch (origin) {
+                    case ZLIB_FILEFUNC_SEEK_SET:
+                    default:
+                        return ISeekableInputStream::Seek::BEGIN;
+                    case ZLIB_FILEFUNC_SEEK_CUR:
+                        return ISeekableInputStream::Seek::CURRENT;
+                    case ZLIB_FILEFUNC_SEEK_END:
+                        return ISeekableInputStream::Seek::END;
+                }
+            }());
+            return 0;
+        },
+        .zclose_file = [](voidpf opaque, voidpf stream) -> int { return 0; },
+        .zerror_file = [](voidpf opaque, voidpf stream) -> int { return 0; },
+        .opaque = stream.ptr(),
+    };
+    auto unzipHandle = unzOpen2("archive.zip", &funcs);
+
+    if (unzipHandle == nullptr) {
+        throw AZLibException("can't open ZipFileReader");
+    }
+    AUI_DEFER { unzClose(unzipHandle); };
+
+    unz_global_info64 info;
+    if (auto err = unzGetGlobalInfo64(unzipHandle, &info)) {
+        throw AZLibException("unzGetGlobalInfo failed: {}"_format(err));
+    }
+    for (size_t i = info.number_entry; i > 0; i--) {
+        char filename[0x400];
+
+        unz_file_info64 fileInfo;
+        if (auto err =
+                unzGetCurrentFileInfo64(unzipHandle, &fileInfo, filename, sizeof(filename), nullptr, 0, nullptr, 0);
+            err != UNZ_OK) {
+            throw AZLibException("unzGetCurrentFileInfo64 failed: {}"_format(err));
+        }
+
+        ZipEntry ze;
+        ze.name = filename;
+        ze.mHandle = unzipHandle;
+        visitor(ze);
+
+        if (i > 1) {
+            unzGoToNextFile(unzipHandle);
+        }
+    }
+}
+
+void aui::zlib::ExtractTo::operator()(const ZipEntry& zipEntry) const {
+    AFileOutputStream(prefix / zipEntry.name) << zipEntry.open();
 }
--- aui.core/src/AUI/Util/LZ.h
+++ aui.core/src/AUI/Util/LZ.h
@@ -15,22 +15,75 @@
 #include "AUI/Common/AException.h"
 #include "AUI/IO/IInputStream.h"
 
+#include <AUI/IO/APath.h>
+#include <AUI/IO/ISeekableInputStream.h>
+
 class AByteBuffer;
 
 class AByteBufferView;
 
 class AZLibException : public AException {
 public:
-    AZLibException() {
-    }
+    AZLibException() {}
 
-    AZLibException(const AString& message)
-            : AException(message) {
-    }
+    AZLibException(const AString& message) : AException(message) {}
 };
 
 namespace aui::zlib {
-    void API_AUI_CORE compress(AByteBufferView b, AByteBuffer& dst);
-    void API_AUI_CORE decompress(AByteBufferView b, AByteBuffer& dst);
-    _unique<IInputStream> API_AUI_CORE decompressToStream(AByteBufferView b);
-}
+void API_AUI_CORE compress(AByteBufferView b, AByteBuffer& dst);
+void API_AUI_CORE decompress(AByteBufferView b, AByteBuffer& dst);
+_unique<IInputStream> API_AUI_CORE decompressToStream(AByteBufferView b);
+
+/**
+ * @brief Zip file entry.
+ */
+struct API_AUI_CORE ZipEntry : aui::noncopyable {
+    friend void API_AUI_CORE
+    readZip(aui::no_escape<ISeekableInputStream> stream, const std::function<void(const ZipEntry&)>& visitor);
+    /**
+     * @brief file path inside ZIP.
+     */
+    std::string_view name;
+
+    struct API_AUI_CORE Stream : IInputStream, aui::noncopyable {
+        friend struct API_AUI_CORE ZipEntry;
+        size_t read(char* dst, size_t size) override;
+        ~Stream() override;
+
+    private:
+        explicit Stream(void* handle) : mHandle(handle) {}
+        void* mHandle = nullptr;
+    };
+
+    /**
+     * @brief Opens the zip entry for read.
+     * @param password
+     * @return
+     */
+    Stream open(const std::string& password = {}) const;
+
+private:
+    void* mHandle;
+};
+
+/**
+ * @brief ZIP reader.
+ * @ingroup io
+ * @param stream ZIP file stream.
+ * @param visitor zip contents visitor.
+ * @details
+ * ZIP file names are passed to callback. Optionally, the callback might request file contents by ZipEntry::open().
+ * @sa aui::zlib::ExtractTo
+ */
+void API_AUI_CORE readZip(aui::no_escape<ISeekableInputStream> stream, const std::function<void(const ZipEntry&)>& visitor);
+
+/**
+ * @brief Default extracter for aui::zlib::readZip.
+ * @ingroup io
+ */
+struct API_AUI_CORE ExtractTo {
+    APath prefix;
+
+    void operator()(const ZipEntry& zipEntry) const;
+};
+}   // namespace aui::zlib
