d770b5ba06d5a22a4e855e706ab62c9d5624420a
alex2772 <alex2772sc@gmail.com>
2025-02-14 23:47:23 +0300

    added docs

--- aui.views/src/AUI/Platform/AMessageBox.h
+++ aui.views/src/AUI/Platform/AMessageBox.h
@@ -44,14 +44,99 @@ class AWindow;
  * @image html revierio.png
  */
 namespace AMessageBox {
-enum class Icon { NONE, INFO, WARNING, CRITICAL };
+
+/**
+ * @brief Specifies icon to be displayed.
+ */
+enum class Icon {
+    /**
+     * @brief No icon is specified.
+     * @details
+     * @specificto{macos}
+     * Displays app's bundle icon.
+     */
+    NONE,
+
+    /**
+     * @brief Information icon.
+     */
+    INFO,
+
+    /**
+     * @brief Warning icon.
+     */
+    WARNING,
+
+    /**
+     * @brief Critical icon.
+     */
+    CRITICAL
+};
+
+/**
+ * @brief Specifies button(s) to be displayed.
+ */
 enum class Button {
+    /**
+     * @brief Display OK button only.
+     */
     OK,
+
+    /**
+     * @brief Display OK and Cancel buttons.
+     */
     OK_CANCEL,
+
+    /**
+     * @brief Display Yes and No buttons.
+     */
     YES_NO,
+
+    /**
+     * @brief Display Yes, No and Cancel buttons.
+     */
     YES_NO_CANCEL,
 };
-enum class ResultButton { INVALID, OK, CANCEL, YES, NO };
+
+/**
+ * @brief Button that the user has clicked.
+ */
+enum class ResultButton {
+    /**
+     * @brief Indicates an invalid or undefined result.
+     */
+    INVALID,
+
+    /**
+     * @brief Indicates the user clicked the OK button.
+     */
+    OK,
+
+    /**
+     * @brief Indicates the user clicked the Cancel button.
+     */
+    CANCEL,
+
+    /**
+     * @brief Indicates the user clicked the Yes button.
+     */
+    YES,
+
+    /**
+     * @brief Indicates the user clicked the No button.
+     */
+    NO,
+};
+
+/**
+ * @brief Displays a message box, blocking the caller thread until the user dismisses the message.
+ * @param parent the window the message box relates to. The OS might fade out the parent window and/or place the message
+ * box within its geometry. Can be `nullptr`.
+ * @param title title of the message box.
+ * @param message content of the message box.
+ * @param icon icon style for the message box. Defaults to NONE.
+ * @param b buttons to be displayed in the message box.
+ */
 API_AUI_VIEWS ResultButton
 show(AWindow* parent, const AString& title, const AString& message, Icon icon = Icon::NONE, Button b = Button::OK);
 };   // namespace AMessageBox
