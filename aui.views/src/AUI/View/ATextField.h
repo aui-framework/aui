1a0822c5b28a19593c50f93e3761c5fcde4128c5
alex2772 <alex2772sc@gmail.com>
2025-03-08 20:50:42 +0300

    ESC now drops focus

--- aui.views/src/AUI/View/ATextField.h
+++ aui.views/src/AUI/View/ATextField.h
@@ -21,28 +21,21 @@
  * @details
  * <img src="https://github.com/aui-framework/aui/raw/master/docs/imgs/ATextField.gif">
  */
-class API_AUI_VIEWS ATextField: public AAbstractTextField
-{
+class API_AUI_VIEWS ATextField : public AAbstractTextField {
 public:
-	ATextField();
-	~ATextField() override;
+    ATextField();
+    ~ATextField() override;
 
 protected:
-	bool isValidText(const AString& text) override;
+    bool isValidText(const AString& text) override;
 };
 
-template<>
+template <>
 struct ADataBindingDefault<ATextField, AString> {
 public:
-    static auto property(const _<ATextField>& view) {
-        return view->text();
-    }
+    static auto property(const _<ATextField>& view) { return view->text(); }
 
     static void setup(const _<ATextField>& view) {}
-    static auto getGetter() {
-        return (ASignal<AString> ATextField::*)&ATextField::textChanging;
-    }
-    static auto getSetter() {
-        return (void(ATextField::*)(const AString& t)) &ATextField::setText;
-    }
+    static auto getGetter() { return (ASignal<AString> ATextField::*) &ATextField::textChanging; }
+    static auto getSetter() { return (void(ATextField::*)(const AString& t)) & ATextField::setText; }
 };
