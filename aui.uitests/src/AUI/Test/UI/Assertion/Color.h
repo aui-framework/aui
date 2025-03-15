05e919c08a0fb9a511b4e5ac9bdf8f4525778499
Alex2772 <alex2772sc@gmail.com>
2025-03-15 06:16:17 +0300

    ASS merge fixes (#452)
    * added test
    
    * fix unset
    
    * fixed unset_wrap move semantics
    
    * tryfix shindows
    
    * tryfix shindows
    
    * added win32 rel with deb info build
    
    * tryfix shindows
    
    * u

--- aui.uitests/src/AUI/Test/UI/Assertion/Color.h
+++ aui.uitests/src/AUI/Test/UI/Assertion/Color.h
@@ -21,9 +21,9 @@
 
 struct ViewAssertionAverageColor {
     AColor mColor;
-    float mInaccuracy;
+    aui::float_within_0_1 mInaccuracy;
 
-    ViewAssertionAverageColor(const AColor& color, float inaccuracy = 0.1f) : mColor(color), mInaccuracy(inaccuracy) {}
+    ViewAssertionAverageColor(const AColor& color, aui::float_within_0_1 inaccuracy = 0.1f) : mColor(color), mInaccuracy(inaccuracy) {}
 
     bool operator()(const _<AView>& v) {
         glm::vec3 average(ScreenshotAnalyzer::makeScreenshot().clip(v).averageColor());
@@ -39,3 +39,35 @@ struct ViewAssertionAverageColor {
  *        the same color.
  */
 using averageColor = ViewAssertionAverageColor;
+
+struct ViewAssertionPixelColorAt {
+    using RelativePosition = glm::vec<2, aui::float_within_0_1>;
+    RelativePosition mRelativePosition;
+    AColor mColor;
+    aui::float_within_0_1 mInaccuracy;
+
+    ViewAssertionPixelColorAt(RelativePosition relativePosition, const AColor& color, aui::float_within_0_1 inaccuracy = 0.1f): mRelativePosition(relativePosition), mColor(color), mInaccuracy(inaccuracy) {}
+
+    bool operator()(const _<AView>& v) {
+        auto screenshot = ScreenshotAnalyzer::makeScreenshot().clip(v);
+        auto mappedPos = glm::uvec2(glm::vec2(screenshot.image().size() - 1u) * glm::vec2(mRelativePosition));
+        auto target = glm::vec3(screenshot.image().get(glm::min(mappedPos, screenshot.image().size() - 1u)));
+        auto d = glm::length2(target - glm::vec3(mColor)) / 3;
+        return d <= mInaccuracy;
+    }
+};
+
+/**
+ * @brief Checks for pixel color on view on the screenshot.
+ * @param relativePosition a vec2 within [0.0;1.0)
+ * @param color the color to check for.
+ * @param inaccuracy [0.0;1.0) color matching coefficient, where 1 = can be unmatched completely, 0 = should be exactly
+ *        the same color.
+ * @details
+ * relativePosition is a coordinate within the matched view(s) coordinate space, multiplied by its size. For example:
+ *
+ * - (0,0) corresponds to the top left corner.
+ * - (1,1) corresponds to the bottom right corner.
+ * - (0.5,0.5) corresponds to the center.
+ */
+using pixelColorAt = ViewAssertionPixelColorAt;
