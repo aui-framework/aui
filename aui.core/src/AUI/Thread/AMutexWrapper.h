72cabd8cb14c31a5bfbe90bb535c71bd1c9fd423
alex2772 <alex2772sc@gmail.com>
2025-05-30 21:11:33 +0300

    wip

--- aui.core/src/AUI/Thread/AMutexWrapper.h
+++ aui.core/src/AUI/Thread/AMutexWrapper.h
@@ -11,11 +11,96 @@
 
 #pragma once
 
-#include "AExclusiveAccess.h"
+#include <AUI/Traits/values.h>
+#include <AUI/Traits/memory.h>
 
 /**
- * @brief Wraps the object with mutex, providing thread-safety layer and a runtime check, see @ref AExclusiveAccess.
+ * @brief Wraps the object with a Lockable, providing exclusive access layer, i.e., for thread-safety.
  * @ingroup core
+ * @tparam T The type of the value to be protected.
+ * @tparam Lockable A lockable type which manages locking behaviour, i.e, AMutex.
+ * @details
+ * On debug builds, provides a runtime check for better diagnostics.
+ *
+ * Aka `boost::synchronized_value`.
+ *
+ * # Implementing thread safety
+ *
+ * @code{cpp}
+ * struct SharedResource {
+ *   AString data;
+ * };
+ * AMutexWrapper<SharedResource, AMutex> sharedResource;
+ * sharedResource->data = "test"; // bad, will crash
+ *
+ * ...
+ * // thread 1
+ * std::unique_lock lock(sharedResource); //
+ * sharedResource->data = "hello";        // ok
+ * ...
+ *
+ * // thread 2
+ * std::unique_lock lock(sharedResource); //
+ * sharedResource->data = "world";        // ok, will be done before or after "hello", but not simultaneously
+ * ...
+ * @endcode
+ *
+ * # Exclusive Access
+ * See @ref ARuntimeBorrowChecker
+ *
  */
-template<typename T>
-using AMutexWrapper = AExclusiveAccess<T, AMutex>;
+template<typename T, typename Lockable = AMutex>
+class AMutexWrapper: public aui::noncopyable {
+public:
+    AMutexWrapper(T value = T()) noexcept: mValue(std::move(value)) {}
+
+
+    void lock() {
+        mMutex.lock();
+
+#if AUI_DEBUG
+        mOwnerThread = AThread::current();
+#endif
+    }
+
+    AMutexWrapper& operator=(const T& rhs) {
+        value() = rhs;
+        return *this;
+    }
+
+    AMutexWrapper& operator=(T&& rhs) noexcept {
+        value() = std::move(rhs);
+        return *this;
+    }
+
+    template<typename U, std::enable_if_t<std::is_constructible_v<U, T>>* = 0>
+    operator U() noexcept {
+        return { value() };
+    }
+
+    void unlock() {
+#if AUI_DEBUG
+        mOwnerThread = nullptr;
+#endif
+        mMutex.unlock();
+    }
+
+    T& value() noexcept {
+#if AUI_DEBUG
+        AUI_ASSERTX(mOwnerThread == AThread::current(), "AMutexWrapper should be locked by this thread in order to get access to the underlying object");
+#endif
+        return mValue;
+    }
+
+    T* operator->() noexcept {
+        return &value();
+    }
+
+private:
+    T mValue;
+    Lockable mMutex;
+
+#if AUI_DEBUG
+    _<AAbstractThread> mOwnerThread;
+#endif
+};
---
+++ aui.core/tests/RuntimeBorrowCheckerTest.cpp
@@ -0,0 +1,97 @@
+// AUI Framework - Declarative UI toolkit for modern C++20
+// Copyright (C) 2020-2025 Alex2772 and Contributors
+//
+// SPDX-License-Identifier: MPL-2.0
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#include <AUI/Common/AProperty.h>
+#include <AUI/Logging/ALogger.h>
+#include <AUI/Util/kAUI.h>
+#include <gmock/gmock.h>
+#include "AUI/ARuntimeBorrowChecker.h"
+
+class RuntimeBorrowChecker : public testing::Test {
+public:
+};
+
+// AUI_DOCS_OUTPUT: doxygen/intermediate/exclusive_access_lockable.h
+// @class ARuntimeBorrowChecker
+
+TEST_F(RuntimeBorrowChecker, Principle) {   // HEADER_H1
+    // AUI_DOCS_CODE_BEGIN
+    struct SharedResource {
+        AString data;
+    };
+    ARuntimeBorrowChecker<SharedResource> sharedResource;
+
+    if (auto read1 = sharedResource.readScope()) {   // ok
+        /* // HIDE
+        read1->data = "Hello"; // can't
+        */ // HIDE
+        [[maybe_unused]] auto length1 = read1->data.length();   // ok
+
+        if (auto read2 = sharedResource.readScope()) {         // ok, second read
+            [[maybe_unused]] auto length2 = read2->data.length();   // ok
+
+            if (auto write = sharedResource.writeScope()) { // bad, there are reads in progress
+                // this branch will not be executed, since we've failed to lock
+                GTEST_FAIL() << "should not reach here"; // HIDE
+            } else {
+                // instead, it will go here.
+                // it is up to you how you want to handle such case.
+                // throw AException("cannot write shared resource");
+            }
+        } else {
+            throw AException("cannot read shared resource");
+        }
+    } else {
+        throw AException("cannot read shared resource");
+    }
+    ok:
+
+    if (auto write = sharedResource.writeScope()) { // ok
+        write->data = "hell";
+    } else {
+        throw AException("can't write to shared resource");
+    }
+    // AUI_DOCS_CODE_END
+    //
+    // Always check if scope acquisition succeeded and handle just in case.
+    //
+    // Use the appropriate scope type (read/write) for intended operations. Prefer read scopes when write access isn't
+    // necessarily needed.
+}
+
+/*
+TEST_F(RuntimeBorrowChecker, Usage) {   // HEADER_H1
+
+    // In this example, we are ensuring the integrity of for each loop iteration:
+    // AUI_DOCS_CODE_BEGIN
+    struct SharedResource {
+        AVector<AString> users;
+    };
+    ARuntimeBorrowChecker<SharedResource> sharedResource;
+
+    auto goodFunction = [] {
+
+    };
+
+    auto handleUsers = [&](const std::function<void(const AString&)>& handler) {
+        if (auto sharedResourceRead = sharedResource.readScope()) {
+            for (const auto& user : sharedResourceRead->users) {
+                handler(user);   // ok, but if handler happens to modify users, it
+                                 // would break our foreach loop
+            }
+        } else {
+            throw AException("cannot read shared resource");
+        }
+    };
+
+    handleUsers(goodFunction);
+
+    // AUI_DOCS_CODE_END
+}
+*/
