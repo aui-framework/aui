ec8294a4736ed73944253304146377c2dd50d1bc
Aliaska <wantaw@mail.ru>
2025-07-17 13:01:27 +0700

    save webp, on webpconfig.

--- aui.image/src/AUI/Image/webp/WebpImageLoader.cpp
+++ aui.image/src/AUI/Image/webp/WebpImageLoader.cpp
@@ -71,6 +71,65 @@ _<AImage> WebpImageLoader::getRasterImage(AByteBufferView buffer) {
 
     return nullptr;
 }
+void WebpImageLoader::save(aui::no_escape<IOutputStream> outputStream, AImageView image, std::shared_ptr<WebPConfig> config, bool bUseRGBA) {
+#if AUI_DEBUG
+    auto configError = WebPValidateConfig((config).get());   // not mandatory, but useful
+#endif
+
+    // Setup the input data
+    WebPPicture pic;
+    if (!WebPPictureInit(&pic)) {
+        throw AException("WebPPictureInit failed");
+    }
+    pic.width = image.width();
+    pic.height = image.height();
+
+    const uint8_t* data = reinterpret_cast<const uint8_t*>(image.data());
+    int stride = image.width() * (bUseRGBA ? 4 : 3);   // RGB|RGBA
+
+    // allocated picture of dimension width x height
+    // if (!WebPPictureAlloc(&pic)) {
+    //    throw AException("WebPPictureAlloc failed");
+    //}
+    if (bUseRGBA) {
+        if (!WebPPictureImportRGBA(&pic, data, stride)) {
+            throw AException("WebPPictureImportRGB failed");
+        }
+    } else {
+        if (!WebPPictureImportRGB(&pic, data, stride)) {
+            throw AException("WebPPictureImportRGB failed");
+        }
+    }
+    // at this point, 'pic' has been initialized as a container,
+    // and can receive the Y/U/V samples.
+    // Alternatively, one could use ready-made import functions like
+    // WebPPictureImportRGB(), which will take care of memory allocation.
+    // In any case, past this point, one will have to call
+    // WebPPictureFree(&pic) to reclaim memory.
+    AUI_DEFER { WebPPictureFree(&pic); };
+
+    // Set up a byte-output write method. WebPMemoryWriter, for instance.
+    WebPMemoryWriter wrt;
+    WebPMemoryWriterInit(&wrt);   // initialize 'wrt'
+    // WebPPictureImportRGB()
+    pic.writer = [](const uint8_t* data, size_t dataSize, const WebPPicture* picture) -> int {
+        try {
+            static_cast<IOutputStream*>(picture->custom_ptr)->write(reinterpret_cast<const char*>(data), dataSize);
+        } catch (const AException& e) {
+            ALogger::err("WebP") << "Failed to write: " << e;
+            return -1;
+        }
+        return dataSize;
+    };
+    pic.custom_ptr = outputStream.ptr();
+    pic.use_argb = (bUseRGBA ? 1 : 0);
+    // Compress!
+    int ok = WebPEncode(config.get(), &pic);   // ok = 0 => error occurred!
+    AUI_DEFER { WebPMemoryWriterClear(&wrt); };
+    if (!ok) {
+        throw AException("WebP encoding failed");
+    }
+}
 
 void WebpImageLoader::save(aui::no_escape<IOutputStream> outputStream, AImageView image, aui::ranged_number<float, 0, 100> quality) {
     // Setup a config, starting form a preset and tuning some additional
