1aed597866e201801cee96ebf3b072d667a4baba
Nelonn <42481486+Nelonn@users.noreply.github.com>
2025-07-25 00:36:29 +0300

    stfu clang-tidy

--- aui.audio/src/AUI/Audio/SincResampler.cpp
+++ aui.audio/src/AUI/Audio/SincResampler.cpp
@@ -74,6 +74,8 @@
 // Note: we're glossing over how the sub-sample handling works with
 // |virtual_source_idx_|, etc.
 
+// NOLINTBEGIN(cppcoreguidelines-avoid-magic-numbers,cppcoreguidelines-pro-bounds-pointer-arithmetic,bugprone-integer-division,bugprone-unhandled-self-assignment)
+
 #ifdef UNSAFE_BUFFERS_BUILD
 // TODO(crbug.com/40285824): Remove this and convert code to safer constructs.
 #pragma allow_unsafe_buffers
@@ -554,3 +556,5 @@ float SincResampler::Convolve_NEON(const int kernel_size,
 #endif
 
 }  // namespace media
+
+// NOLINTEND(cppcoreguidelines-avoid-magic-numbers,cppcoreguidelines-pro-bounds-pointer-arithmetic,bugprone-integer-division,bugprone-unhandled-self-assignment)
--- aui.audio/src/AUI/Audio/SincResampler.h
+++ aui.audio/src/AUI/Audio/SincResampler.h
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// NOLINTBEGIN(cppcoreguidelines-avoid-magic-numbers,cppcoreguidelines-pro-bounds-pointer-arithmetic,bugprone-integer-division,bugprone-unhandled-self-assignment)
+
 #ifndef MEDIA_BASE_SINC_RESAMPLER_H_
 #define MEDIA_BASE_SINC_RESAMPLER_H_
 
@@ -321,3 +323,5 @@ private:
 }  // namespace media
 
 #endif  // MEDIA_BASE_SINC_RESAMPLER_H_
+
+// NOLINTEND(cppcoreguidelines-avoid-magic-numbers,cppcoreguidelines-pro-bounds-pointer-arithmetic,bugprone-integer-division,bugprone-unhandled-self-assignment)
