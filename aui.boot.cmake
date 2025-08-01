ff0b379cd1e60240fe6f267b30952be74a147130
mikle <mishaiordanskiy@gmail.com>
2025-08-02 02:01:03 +0400

    fix

--- aui.boot.cmake
+++ aui.boot.cmake
@@ -1159,15 +1159,12 @@ function(auib_import AUI_MODULE_NAME URL)
     endif()
     _auib_find_git()
     if (GIT_EXECUTABLE)
-        #execute_process(COMMAND ${GIT_EXECUTABLE} rev-parse --short HEAD rev-parse --abbrev-ref HEAD
-        #        OUTPUT_VARIABLE git_hash_or_tag
-        #)
         execute_process(COMMAND ${GIT_EXECUTABLE} status
                 WORKING_DIRECTORY ${DEP_SOURCE_DIR}
                 OUTPUT_VARIABLE git_status
         )
         if(NOT git_status MATCHES "HEAD")
-            message(WARNING "You are staying on a branch or did not specify the version control, please specify a tag or hash VERSION ")
+            message(WARNING "${AUIB_IMPORT_NAME} You are staying on a branch or did not specify the version control, please specify a tag or hash VERSION!\nSee https://aui-framework.github.io/develop/md_docs_2AUI_01Boot.html#version")
         endif ()
     endif ()
 endfunction()
