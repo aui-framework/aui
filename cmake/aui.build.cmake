675edf617893f5253a59d96b0eab381eb651ce07
Saikari <lin@sz.cn.eu.org>
2025-08-14 03:51:21 +0300

    Silence clang++: warning: argument unused during compilation: '-s' [-Wunused-command-line-argument] Use `llvm-strip` (#566)

--- cmake/aui.build.cmake
+++ cmake/aui.build.cmake
@@ -1130,6 +1130,29 @@ function(aui_module AUI_MODULE_NAME)
             COMMENT "Stripping ${AUI_MODULE_NAME} (only for Release/MinSizeRel)"
             VERBATIM
         )
+    elseif (CMAKE_CXX_COMPILER_ID MATCHES "Clang")
+        if(APPLE)
+            add_custom_command(
+                TARGET ${AUI_MODULE_NAME}
+                POST_BUILD
+                COMMAND $<$<OR:$<CONFIG:Release>,$<CONFIG:MinSizeRel>>:${CMAKE_STRIP}>
+                ARGS    $<$<OR:$<CONFIG:Release>,$<CONFIG:MinSizeRel>>:-Sxl>
+                        $<$<OR:$<CONFIG:Release>,$<CONFIG:MinSizeRel>>:$<TARGET_FILE:${AUI_MODULE_NAME}>>
+                COMMENT "Stripping ${AUI_MODULE_NAME} (only for Release/MinSizeRel)"
+                VERBATIM
+            )
+        else()
+            add_custom_command(
+                TARGET ${AUI_MODULE_NAME}
+                POST_BUILD
+                COMMAND $<$<OR:$<CONFIG:Release>,$<CONFIG:MinSizeRel>>:${CMAKE_STRIP}>
+                ARGS    $<$<OR:$<CONFIG:Release>,$<CONFIG:MinSizeRel>>:-g>
+                        $<$<OR:$<CONFIG:Release>,$<CONFIG:MinSizeRel>>:-x>
+                        $<$<OR:$<CONFIG:Release>,$<CONFIG:MinSizeRel>>:$<TARGET_FILE:${AUI_MODULE_NAME}>>
+                COMMENT "Stripping ${AUI_MODULE_NAME} (only for Release/MinSizeRel)"
+                VERBATIM
+            )
+        endif()
     endif()
 endfunction(aui_module)
 
@@ -1734,7 +1757,7 @@ endmacro()
 
 if (MINGW OR UNIX)
     # strip for release
-    if (NOT(CMAKE_CXX_COMPILER_ID MATCHES "AppleClang"))
+    if (NOT(CMAKE_CXX_COMPILER_ID MATCHES "(AppleClang|Clang)"))
 	    set(CMAKE_C_FLAGS_RELEASE "${CMAKE_C_FLAGS_RELEASE} -s")
 	    set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} -s")
         set(CMAKE_C_FLAGS_MINSIZEREL "${CMAKE_C_FLAGS_MINSIZEREL} -s")
