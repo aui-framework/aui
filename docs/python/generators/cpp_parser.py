16de9f540f1960dbbb26e7053b4d3ebff9fef1d6
alex2772 <alex2772sc@gmail.com>
2025-08-17 18:30:45 +0300

    cpp_parser.py: migrate to class

--- docs/python/generators/cpp_parser.py
+++ docs/python/generators/cpp_parser.py
@@ -18,7 +18,7 @@ import os
 import re
 from pathlib import Path
 
-from docs.python.generators import regexes, cpp_tokenizer
+from docs.python.generators import cpp_tokenizer
 
 CPP_CLASS_DEF = re.compile('class( API_\S+)? ([a-zA-Z0-9_$]+)')
 assert CPP_CLASS_DEF.match('class Test').group(2) == "Test"
@@ -98,170 +98,169 @@ class CppClass:
     def __str__(self):
         return f'CppClass{self.tuple()}'
 
-def _parse(input: str, location = None | Path):
-    tokens = cpp_tokenizer.tokenize(input)
-    iterator = iter(tokens)
-    last_doc = None
+class _Parser:
+    def __init__(self, input: str, location = None | Path):
+        self.tokens = cpp_tokenizer.tokenize(input)
+        self.iterator = iter(self.tokens)
+        self.last_doc = None
+        self.location = location
+        self.last_token = None
 
-    for token in iterator:
-        def _parse_comment():
-            nonlocal last_doc
-            nonlocal token
-            if token[0] == cpp_tokenizer.Type.COMMENT:
-                if not "/**" in token[1]:
-                    return True
-                last_doc = parse_comment_lines(token[1].split('\n'))
+    def _parse_comment(self):
+        if self.last_token[0] == cpp_tokenizer.Type.COMMENT:
+            if not "/**" in self.last_token[1]:
                 return True
-            return False
+            self.last_doc = parse_comment_lines(self.last_token[1].split('\n'))
+            return True
+        return False
 
-        def _consume_comment():
-            nonlocal last_doc
-            doc = last_doc
-            last_doc = None
-            return doc
 
-        def _skip_special_clause(open = [ cpp_tokenizer.Type.GENERIC_OPEN ], close = [ cpp_tokenizer.Type.GENERIC_CLOSE ]):
-            nonlocal token
-            nonlocal iterator
-            assert token[0] in open
-            special_open = 1
-            out = [ token ]
-            while special_open > 0:
-                token = next(iterator)
-                out.append(token)
-                if token[0] in open:
-                    special_open += 1
-                elif token[0] in close:
-                    special_open -= 1
-            return out
+    def _consume_comment(self):
+        doc = self.last_doc
+        self.last_doc = None
+        return doc
 
-        def _parse_type():
-            nonlocal token
-            nonlocal iterator
-            out = token[1]
-            while token[1] in ['const', 'volatile']:
-                token = next(iterator)
-                out += " " + token[1]
 
-            token = next(iterator)
+    def _skip_special_clause(self, open = [ cpp_tokenizer.Type.GENERIC_OPEN ], close = [ cpp_tokenizer.Type.GENERIC_CLOSE ]):
+        assert self.last_token[0] in open
+        special_open = 1
+        out = [ self.last_token ]
+        while special_open > 0:
+            self.last_token = next(self.iterator)
+            out.append(self.last_token)
+            if self.last_token[0] in open:
+                special_open += 1
+            elif self.last_token[0] in close:
+                special_open -= 1
+        return out
+    def _parse_type(self):
+        out = self.last_token[1]
+        while self.last_token[1] in ['const', 'volatile']:
+            self.last_token = next(self.iterator)
+            out += " " + self.last_token[1]
 
-            # handle namespace types
-            while token[1] == '::':
-                out += token[1]
-                token = next(iterator)
-                out += token[1]
-                token = next(iterator)
+        self.last_token = next(self.iterator)
 
-            is_decltype = 'decltype' in out
-            is_template_wizardy = token[0] in [ cpp_tokenizer.Type.GENERIC_OPEN, cpp_tokenizer.Type.GENERIC_OPEN2 ] and not token[1] == '('
+        # handle namespace types
+        while self.last_token[1] == '::':
+            out += self.last_token[1]
+            self.last_token = next(self.iterator)
+            out += self.last_token[1]
+            self.last_token = next(self.iterator)
 
-            if is_decltype or is_template_wizardy:
-                out += "".join([i[1] for i in _skip_special_clause([ cpp_tokenizer.Type.GENERIC_OPEN, cpp_tokenizer.Type.GENERIC_OPEN2 ], [ cpp_tokenizer.Type.GENERIC_CLOSE, cpp_tokenizer.Type.GENERIC_CLOSE2 ])])
-                token = next(iterator)
-            while token[1] in '&*':
-                out += token[1]
-                token = next(iterator)
-            return out
-        def _parse_class_body(clazz: CppClass):
-            nonlocal iterator
-            nonlocal token
-            assert token[1] == '{'
+        is_decltype = 'decltype' in out
+        is_template_wizardy = self.last_token[0] in [ cpp_tokenizer.Type.GENERIC_OPEN, cpp_tokenizer.Type.GENERIC_OPEN2 ] and not self.last_token[1] == '('
 
-            if clazz.doc is None:
-                _skip_special_clause()
-                return
+        if is_decltype or is_template_wizardy:
+            out += "".join([i[1] for i in self._skip_special_clause([ cpp_tokenizer.Type.GENERIC_OPEN, cpp_tokenizer.Type.GENERIC_OPEN2 ], [ cpp_tokenizer.Type.GENERIC_CLOSE, cpp_tokenizer.Type.GENERIC_CLOSE2 ])])
+            self.last_token = next(self.iterator)
+        while self.last_token[1] in '&*':
+            out += self.last_token[1]
+            self.last_token = next(self.iterator)
+        return out
 
-            visibility = 'private'
-            template_clause = None
+    def _parse_class_body(self, clazz: CppClass):
+        assert self.last_token[1] == '{'
 
-            for token in iterator:
-                # print(f'_parse_class_body iteration {token}')
-                if _parse_comment():
+        if clazz.doc is None:
+            self._skip_special_clause()
+            return
+
+        visibility = 'private'
+        template_clause = None
+
+        for self.last_token in self.iterator:
+            # print(f'_parse_class_body iteration {self.last_token}')
+            if self._parse_comment():
+                continue
+            if self.last_token[1] == '}':
+                break
+            if self.last_token[1] in ['public', 'private', 'protected', 'signals']:
+                visibility = self.last_token[1]
+                if visibility == 'signals':
+                    visibility = 'public'
+                assert next(self.iterator)[1] == ':'
+                continue
+            if self.last_token[0] == cpp_tokenizer.Type.GENERIC_OPEN:
+                self._skip_special_clause()
+
+            if self.last_token[0] == cpp_tokenizer.Type.IDENTIFIER:
+                if self.last_token[1] == 'enum':
+                    while self.last_token[1] != ';':
+                        self.last_token = next(self.iterator)
                     continue
-                if token[1] == '}':
+                if self.last_token[1] == 'template':
+                    template_clause = [self.last_token]
+                    self.last_token = next(self.iterator)
+                    template_clause += self._skip_special_clause([ cpp_tokenizer.Type.GENERIC_OPEN, cpp_tokenizer.Type.GENERIC_OPEN2 ], [ cpp_tokenizer.Type.GENERIC_CLOSE, cpp_tokenizer.Type.GENERIC_CLOSE2 ])
+                    continue
+
+                modifiers_before = []
+                while True:
+                    type_str = self._parse_type()
+                    if type_str in ["explicit", "static", "constexpr", "virtual"]:
+                        modifiers_before.append(type_str)
+                        continue
                     break
-                if token[1] in ['public', 'private', 'protected', 'signals']:
-                    visibility = token[1]
-                    if visibility == 'signals':
-                        visibility = 'public'
-                    assert next(iterator)[1] == ':'
+                if self.last_token[1] == '(':
+                    # ctor
+                    clazz.methods.append(CppFunction(name=type_str, doc=self._consume_comment(), visibility=visibility, args=self._skip_special_clause(), modifiers_before=modifiers_before))
                     continue
-                if token[0] == cpp_tokenizer.Type.GENERIC_OPEN:
-                    _skip_special_clause()
-
-                if token[0] == cpp_tokenizer.Type.IDENTIFIER:
-                    if token[1] == 'enum':
-                        while token[1] != ';':
-                            token = next(iterator)
+                elif self.last_token[0] == cpp_tokenizer.Type.IDENTIFIER:
+                    name = self.last_token[1]
+                    self.last_token = next(self.iterator)
+                    if self.last_token[1] == ';':
+                        clazz.fields.append(CppVariable(name=name, type_str=type_str, doc=self._consume_comment(), visibility=visibility))
                         continue
-                    if token[1] == 'template':
-                        template_clause = [token]
-                        token = next(iterator)
-                        template_clause += _skip_special_clause([ cpp_tokenizer.Type.GENERIC_OPEN, cpp_tokenizer.Type.GENERIC_OPEN2 ], [ cpp_tokenizer.Type.GENERIC_CLOSE, cpp_tokenizer.Type.GENERIC_CLOSE2 ])
-                        continue
-
-                    modifiers_before = []
-                    while True:
-                        type_str = _parse_type()
-                        if type_str in ["explicit", "static", "constexpr", "virtual"]:
-                            modifiers_before.append(type_str)
-                            continue
+                    if self.last_token[1] == '(':
+                        clazz.methods.append(CppFunction(name=name, return_type=type_str, doc=self._consume_comment(), visibility=visibility, template_clause=template_clause, args=self._skip_special_clause(), modifiers_before=modifiers_before))
+                        template_clause = None
+                        self.last_token = next(self.iterator)
+                while True:
+                    if self.last_token[1] == ';':
                         break
-                    if token[1] == '(':
-                        # ctor
-                        clazz.methods.append(CppFunction(name=type_str, doc=_consume_comment(), visibility=visibility, args=_skip_special_clause(), modifiers_before=modifiers_before))
-                        continue
-                    elif token[0] == cpp_tokenizer.Type.IDENTIFIER:
-                        name = token[1]
-                        token = next(iterator)
-                        if token[1] == ';':
-                            clazz.fields.append(CppVariable(name=name, type_str=type_str, doc=_consume_comment(), visibility=visibility))
-                            continue
-                        if token[1] == '(':
-                            clazz.methods.append(CppFunction(name=name, return_type=type_str, doc=_consume_comment(), visibility=visibility, template_clause=template_clause, args=_skip_special_clause(), modifiers_before=modifiers_before))
-                            template_clause = None
-                            token = next(iterator)
-                    while True:
-                        if token[1] == ';':
-                            break
-                        if token[1] == '{':
-                            _skip_special_clause()
-                            break
-                        # consume const noexcept
-                        token = next(iterator)
-                    _consume_comment()
+                    if self.last_token[1] == '{':
+                        self._skip_special_clause()
+                        break
+                    # consume const noexcept
+                    self.last_token = next(self.iterator)
+                self._consume_comment()
 
+    def parse(self):
+        for self.last_token in self.iterator:
+            prev_doc = self.last_doc
+            if self._parse_comment():
+                if prev_doc is not None:
+                    entry = DoxygenEntry(doc=prev_doc)
+                    entry.location = self.location
+                    yield entry
+                continue
 
-        prev_doc = last_doc
-        if _parse_comment():
-            if prev_doc is not None:
-                entry = DoxygenEntry(doc=prev_doc)
-                entry.location = location
-                yield entry
-            continue
+            if self.last_token == (cpp_tokenizer.Type.IDENTIFIER, 'enum'):
+                self.last_token = next(self.iterator) # TODO parse enums
+                continue
 
-        if token == (cpp_tokenizer.Type.IDENTIFIER, 'enum'):
-            token = next(iterator) # TODO parse enums
-            continue
+            if self.last_token == (cpp_tokenizer.Type.IDENTIFIER, 'class'):
+                self.last_token = next(self.iterator)
+                if self.last_token[1].startswith("API_"):
+                    # export macro, ignore
+                    self.last_token = next(self.iterator)
+                assert self.last_token[0] == cpp_tokenizer.Type.IDENTIFIER
+                clazz = CppClass()
+                clazz.name = self.last_token[1]
+                clazz.doc = self._consume_comment()
+                clazz.location = self.location
 
-        if token == (cpp_tokenizer.Type.IDENTIFIER, 'class'):
-            token = next(iterator)
-            if token[1].startswith("API_"):
-                # export macro, ignore
-                token = next(iterator)
-            assert token[0] == cpp_tokenizer.Type.IDENTIFIER
-            clazz = CppClass()
-            clazz.name = token[1]
-            clazz.doc = _consume_comment()
-            clazz.location = location
+                while self.last_token[1] not in '{;':
+                    self.last_token = next(self.iterator)
+                if self.last_token[1] == '{':
+                    self._parse_class_body(clazz)
 
-            while token[1] not in '{;':
-                token = next(iterator)
-            if token[1] == '{':
-                _parse_class_body(clazz)
+                if clazz.doc:
+                    yield clazz
 
-            if clazz.doc:
-                yield clazz
+def _parse(input: str, location = None | Path):
+    return _Parser(input=input, location=location).parse()
 
 def _scan():
     contents = []
