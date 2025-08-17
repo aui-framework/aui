8c3a2e20af01e2a619ba76683edd5319cbc5cb1d
alex2772 <alex2772sc@gmail.com>
2025-08-17 12:32:01 +0300

    autorefs parser state machine

--- docs/python/generators/autorefs.py
+++ docs/python/generators/autorefs.py
@@ -37,29 +37,63 @@ def handle_autorefs(markdown: str, page : Page, files: Files):
     if _mapping is None:
         _mapping = {_extract_page_title(i): i for i in files} | { i.src_uri : i for i in files }
 
-    def replace_url(match: re.Match):
-        referred_page_id = match.group(1)
+    def replace_url(referred_page_id: str):
         referred_page = find_page(referred_page_id)
         if not referred_page:
-            line = markdown[:match.endpos].count('\n') + 1
-            log.warning(f"Doc file '{page.file.abs_src_path}' (line {line}) contains an unrecognized explicit link to '{referred_page_id}'.")
+            log.warning(f"Doc file '{page.file.abs_src_path}' contains an unrecognized explicit link to '{referred_page_id}'.")
             return f'<span style="background:red">unrecognized link to {referred_page_id}</span>'
         title = _extract_page_title(referred_page)
         return f"[{title}]({page.file.src_uri})"
 
-    def handle_lines(iterator: iter):
-        for line in iterator:
-            if "```" in line:
-                yield line
-                for line in iterator:
-                    yield line
-                    if "```" in line:
-                        break
-            else:
-                line = re.sub(
-                    r"\[(.+)\](?![\(])",
-                    replace_url, line, flags=re.I | re.M
-                )
-                yield line
+    def tokenize():
+        iterator = iter(markdown)
+        for i in iterator:
+            match i:
+                case '`':
+                    i = next(iterator)
+                    if i == '`':
+                        i = next(iterator)
+                        if i == '`':
+                            yield '```'
+                        else:
+                            yield '`'
+                            yield '`'
+                            yield i
+                    else:
+                        yield '`'
+                        yield i
+                case _:
+                    yield i
 
-    return "\n".join([i for i in handle_lines(iter(markdown.splitlines()))])
+    def parse():
+        iterator = iter(tokenize())
+        state_code = False
+        for i in iterator:
+            if i == '```' or i == '`':
+                yield i
+                state_code = not state_code
+            elif not state_code and i == '[':
+                link_first_part = ''
+                for i in iterator:
+                    if i == ']':
+                        break
+                    link_first_part += i
+                try:
+                    i = next(iterator)
+                except StopIteration:
+                    i = ''
+                if i == '(':
+                    # just a regular link, no special treatment
+                    yield f"[{link_first_part}]("
+                else:
+                    # an explicit link in format [AObject], where AObject is a destination classname/page name.
+                    # this is not a standard Markdown though.
+                    yield replace_url(link_first_part)
+                    yield i
+
+            else:
+                yield i
+
+
+
+    return "".join([i for i in parse()])
