80192c772bc5f52a5aaee99f407791d0ede490e4
adavidys <zazadavidzaza2281337@gmail.com>
2025-08-29 17:09:23 +0300

    ZXC division of responsibility ZXC

--- docs/python/generators/examples_page.py
+++ docs/python/generators/examples_page.py
@@ -19,6 +19,40 @@ from pathlib import Path
 from docs.python.generators import regexes, common
 
 
+def collect_srcs(top):
+    for root, dirs, files in os.walk(top):
+        for f in files:
+            if any(f.endswith(ext) for ext in ['.h', 'cpp', 'CMakeLists.txt']):
+                yield Path(root) / f
+
+def get_id(title: str) -> str:
+    if "{" in title:
+        id = title[title.find("{#")+2:title.find("}")]
+        title = title[:title.find("{#")]
+    else:
+        id = title.lower()
+        for i in [" ", "_"]:
+            id = id.replace(i, "-")
+        id = "".join(ch for ch in id if ch.isalnum() or ch == '-')
+        id = id.strip('-')
+
+    return id
+
+def descriptions(readlines: list[str]) -> tuple[str | None, str]:
+    category = None
+    description = ""
+    it = iter(readlines)
+    for line in it:
+        if m := regexes.AUI_EXAMPLE.match(line):
+            category = m.group(1)
+            for description_line in it:
+                description_line = description_line.strip("\n")
+                if not description_line:
+                    break
+                description += " " + description_line
+
+    return category, description.strip()
+
 def examine():
     EXAMPLES_DIR = Path.cwd() / "examples"
     EXAMPLES_DIR = EXAMPLES_DIR.as_posix()
@@ -27,20 +61,11 @@ def examine():
 
     for root, dirs, files in os.walk(EXAMPLES_DIR):
         for file in files:
-            if file != "README.md":
-                continue
-
             example_path = str(Path(root).relative_to(EXAMPLES_DIR))
-            if "/" not in example_path and "\\" not in example_path:
+            if file != "README.md" or os.sep not in example_path:
                 continue
 
             example_path = example_path.replace(os.sep, "_")
-
-            def collect_srcs(top):
-                for root, dirs, files in os.walk(top):
-                    for f in files:
-                        if any(f.endswith(ext) for ext in ['.h', 'cpp', 'CMakeLists.txt']):
-                            yield Path(root) / f
 
             srcs = list(collect_srcs(root))
 
@@ -49,29 +74,10 @@ def examine():
                 title = fis.readline()
                 assert title.startswith("# ")
                 title = title.lstrip("# ").rstrip("\n")
-                if "{" in title:
-                    id = title[title.find("{#")+2:title.find("}")]
-                    title = title[:title.find("{#")]
-                else:
-                    id = title.lower()
-                    for i in [" ", "_"]:
-                        id = id.replace(i, "-")
-                    id = "".join(ch for ch in id if ch.isalnum() or ch == '-')
-                    id = id.strip('-')
 
-                category = None
-                description = ""
-                it = iter(fis.readlines())
-                for line in it:
-                    if m := regexes.AUI_EXAMPLE.match(line):
-                        category = m.group(1)
-                        for description_line in it:
-                            description_line = description_line.strip("\n")
-                            if not description_line:
-                                break
-                            description += " " + description_line
+                id = get_id(title)
+                category, description = descriptions(fis.readlines())
 
-                description = description.strip()
                 if not id:
                     raise RuntimeError(f"no id provided in {input_file}")
                 if not category:
