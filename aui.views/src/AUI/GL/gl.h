5d127821d9cdbab048759a03785df159e1cfa8b2
Nelonn <42481486+Nelonn@users.noreply.github.com>
2025-10-18 23:48:13 +0400

    Fixed opengl header

--- aui.views/src/AUI/GL/gl.h
+++ aui.views/src/AUI/GL/gl.h
@@ -12,22 +12,3 @@
 #pragma once
 
 #include <glad/glad.h>
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-#if AUI_PLATFORM_ANDROID
-#include <GLES3/gl3.h>
-#include <GLES3/gl31.h>
-#include <GLES3/gl32.h>
-#include <GLES3/gl3ext.h>
-#include <EGL/egl.h>
-#elif AUI_PLATFORM_MACOS
-#include <OpenGL/OpenGL.h>
-#elif AUI_PLATFORM_IOS
-#include <OpenGLES/ES3/gl.h>
-#include <OpenGLES/ES3/glext.h>
-#endif
-#ifdef __cplusplus
-}
-#endif
