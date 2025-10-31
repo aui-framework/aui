aba7b61e79bbca2a28486c872830aa7463c9d338
Saikari <lin@sz.cn.eu.org>
2025-10-31 17:08:10 +0300

    remove shared GLEW configuration and platform-specific defines from minimal UI example

--- examples/ui/minimal_ui_xmake/xmake.lua
+++ examples/ui/minimal_ui_xmake/xmake.lua
@@ -8,8 +8,6 @@ set_languages("c++20")
 
 -- Download aui package to use for targets later
 add_requires("aui")
--- Use shared GLEW to resolve LNK2019 errors
-add_requireconfs("**.glew", {override = true, configs = {shared = true}})
 
 -- Define our target executable to build
 target("aui-minimal-example")
@@ -18,22 +16,3 @@ target("aui-minimal-example")
     add_includedirs("src")
     -- Add AUI package to target while linking only required components
     add_packages("aui", {components = {"core", "image", "views", "xml"}})
-    -- Add defines that point to your OS and link against required syslinks/frameworks
-    if is_plat("windows") then
-        add_defines(
-            "AUI_PLATFORM_WIN=1", "AUI_PLATFORM_LINUX=0", "AUI_PLATFORM_APPLE=0"
-        )
-        add_syslinks("gdi32", "ole32")
-    elseif is_plat("linux") then
-        add_defines(
-            "AUI_PLATFORM_WIN=0", "AUI_PLATFORM_LINUX=1", "AUI_PLATFORM_APPLE=0"
-        )
-    elseif is_plat("macosx") then
-        add_defines(
-            "AUI_PLATFORM_WIN=0", "AUI_PLATFORM_LINUX=0", "AUI_PLATFORM_APPLE=1"
-        )
-    end
-    -- Add defines that point to symbol exports
-    add_defines(
-        "API_AUI_VIEWS=AUI_IMPORT", "API_AUI_IMAGE=AUI_IMPORT"
-    )
