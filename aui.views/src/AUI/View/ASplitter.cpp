cd298b6412f648ad1e063b2bcda3b5c46e509fda
Nelonn <42481486+Nelonn@users.noreply.github.com>
2026-05-09 18:19:26 +0400

    provide custom layout in ASplitter

--- aui.views/src/AUI/View/ASplitter.cpp
+++ aui.views/src/AUI/View/ASplitter.cpp
@@ -9,12 +9,10 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
-#include <range/v3/view/transform.hpp>
-
 #include "ASplitter.h"
 #include <AUI/Util/UIBuildingHelpers.h>
-#include <AUI/Util/SplitterSizeInjector.h>
-#include <AUI/Layout/HVLayout.h>
+#include <limits>
+#include <vector>
 
 ASplitter::ASplitter() {}
 
@@ -58,49 +56,381 @@ void ASplitter::onClickPrevented() {
 }
 
 namespace {
-template <aui::derived_from<ALayout> BaseLayout>
-class ASplitterLayout final : public BaseLayout {
+template <ALayoutDirection Direction>
+class ASplitterLayout final : public ALayout {
 public:
-    ranges::range auto viewsWithFixedSizeInjected() {
-        return mSplitterHelper.items() |
-               ranges::views::transform([](const ASplitterHelper::Item& item) { return SizeInjector<BaseLayout::DIRECTION>{ item }; });
-    }
-
     ASplitterLayout(ASplitterHelper& splitterHelper) : mSplitterHelper(splitterHelper) {}
 
-    using HVLayout = aui::HVLayout<BaseLayout::DIRECTION>;
+    void setSpacing(int spacing) override {
+        if (mSpacing == spacing) {
+            return;
+        }
+        mSpacing = spacing;
+        ALayout::requestLayout();
+    }
+
+    void addView(const _<AView>& view, AOptional<size_t> index = std::nullopt) override {
+        auto at = mViews.end();
+        if (index) {
+            at = mViews.begin() + *index;
+        }
+        mViews.insert(at, view);
+        mSplitterHelper.setItems(mViews);
+        ALayout::requestLayout();
+    }
+
+    void removeView(aui::no_escape<AView> view, size_t index) override {
+        AUI_ASSERT(mViews[index].get() == view.ptr());
+        mViews.removeAt(index);
+        mSplitterHelper.setItems(mViews);
+        ALayout::requestLayout();
+    }
+
+    AVector<_<AView>> getAllViews() override {
+        return mViews;
+    }
+
+    ALayoutDirection getLayoutDirection() override {
+        return Direction;
+    }
 
     void layout(int x, int y, int width, int height) override {
-        glm::ivec2 paddedPos = { x, y };
-        glm::ivec2 paddedSize = { width, height };
-
-        // reclaim space if provided size is less than the current layout size.
-        if constexpr (BaseLayout::DIRECTION == ALayoutDirection::HORIZONTAL) {
-            const auto splitterWidth =
-                HVLayout::preferredWidth(viewsWithFixedSizeInjected(), BaseLayout::getSpacing(), height);
-            if (width < splitterWidth) {
-                mSplitterHelper.reclaimSpace(width - splitterWidth);
-            }
-        }
-        if constexpr (BaseLayout::DIRECTION == ALayoutDirection::VERTICAL) {
-            const auto splitterHeight =
-                HVLayout::preferredHeight(viewsWithFixedSizeInjected(), BaseLayout::getSpacing(), width);
-            if (height < splitterHeight) {
-                mSplitterHelper.reclaimSpace(height - splitterHeight);
-            }
+        for (const auto& view : mViews) {
+            view->ensureAssUpdated();
         }
 
-        HVLayout::layout(paddedPos, paddedSize, viewsWithFixedSizeInjected(), BaseLayout::getSpacing());
+        const auto resolved = resolveMainAxisSizes(
+            getAxisValue(glm::ivec2 { width, height }),
+            getPerpAxisValue(glm::ivec2 { width, height }),
+            true,
+            false);
+
+        int position = getAxisValue(glm::ivec2 { x, y });
+        std::size_t index = 0;
+        for (const auto& view : mViews) {
+            const auto& item = resolved[index++];
+            if (!item.visible) {
+                continue;
+            }
+
+            const auto margin = view->getMargin();
+            const int availablePerpendicular = std::max(
+                0,
+                getPerpAxisValue(glm::ivec2 { width, height } - margin.occupiedSize()));
+            const int perpendicularSize = computePerpendicularSize(view, item.mainSize, availablePerpendicular);
+
+            const int positionMain = position + getAxisValue(margin.leftTop());
+            const int positionPerp = getPerpAxisValue(glm::ivec2 { x, y } + margin.leftTop());
+
+            if constexpr (Direction == ALayoutDirection::HORIZONTAL) {
+                view->layout(positionMain, positionPerp, item.mainSize, perpendicularSize);
+            } else {
+                view->layout(positionPerp, positionMain, perpendicularSize, item.mainSize);
+            }
+
+            position += item.mainSize + getAxisValue(margin.occupiedSize()) + mSpacing;
+        }
+    }
+
+    glm::ivec2 onIntrinsicMeasure(AConstraints constraints) override {
+        const bool boundedMain = getAxisValue(glm::ivec2 { constraints.maxWidth, constraints.maxHeight }) < 1000000;
+        const int perpendicularConstraint =
+            getPerpAxisValue(glm::ivec2 { constraints.maxWidth, constraints.maxHeight }) >= 1000000
+                ? -1
+                : getPerpAxisValue(glm::ivec2 { constraints.maxWidth, constraints.maxHeight });
+        const int availableMain = boundedMain
+            ? getAxisValue(glm::ivec2 { constraints.maxWidth, constraints.maxHeight })
+            : 0;
+
+        const auto resolved = resolveMainAxisSizes(
+            availableMain,
+            perpendicularConstraint,
+            boundedMain,
+            !boundedMain);
+
+        glm::ivec2 result {};
+        for (std::size_t i = 0; i < mViews.size(); ++i) {
+            const auto& view = mViews[i];
+            const auto& item = resolved[i];
+            if (!item.visible) {
+                continue;
+            }
+            const auto margin = view->getMargin().occupiedSize();
+            if constexpr (Direction == ALayoutDirection::HORIZONTAL) {
+                result.x += item.mainSize + margin.x;
+                result.y = std::max(result.y, computePerpendicularSize(view, item.mainSize, perpendicularConstraint) + margin.y);
+            } else {
+                result.x = std::max(result.x, computePerpendicularSize(view, item.mainSize, perpendicularConstraint) + margin.x);
+                result.y += item.mainSize + margin.y;
+            }
+        }
+        const int totalSpacing = std::max(0, int(countVisibleItems()) - 1) * mSpacing;
+        getAxisValue(result) += totalSpacing;
+        return result;
     }
 
     AMinMaxSizes onComputeIntrinsicMinMaxSizes(int) override {
-        return HVLayout::computeIntrinsicMinMaxSizes(viewsWithFixedSizeInjected(), BaseLayout::getSpacing());
+        AMinMaxSizes result;
+        for (const auto& view : mViews) {
+            view->ensureAssUpdated();
+            if (!(view->getVisibility() & Visibility::FLAG_CONSUME_SPACE)) {
+                continue;
+            }
+
+            const auto minMax = view->computeMinMaxSizes();
+            const auto margin = view->getMargin().occupiedSize();
+            const int minMain = computeMinimumMainAxisSize(view, -1);
+            const int maxMain = computePreferredMainAxisSize(view, -1, true);
+            const int minPerp = computePerpendicularSize(view, minMain, -1);
+            const int maxPerp = computePerpendicularSize(view, maxMain, -1);
+
+            if constexpr (Direction == ALayoutDirection::HORIZONTAL) {
+                result.min.x += minMain + margin.x;
+                result.max.x += maxMain + margin.x;
+                result.min.y = std::max(result.min.y, std::max(minPerp, minMax.min.y) + margin.y);
+                result.max.y = std::max(result.max.y, std::max(maxPerp, minMax.min.y) + margin.y);
+            } else {
+                result.min.x = std::max(result.min.x, std::max(minPerp, minMax.min.x) + margin.x);
+                result.max.x = std::max(result.max.x, std::max(maxPerp, minMax.min.x) + margin.x);
+                result.min.y += minMain + margin.y;
+                result.max.y += maxMain + margin.y;
+            }
+        }
+        const int totalSpacing = std::max(0, int(countVisibleItems()) - 1) * mSpacing;
+        if constexpr (Direction == ALayoutDirection::HORIZONTAL) {
+            result.min.x += totalSpacing;
+            result.max.x += totalSpacing;
+        } else {
+            result.min.y += totalSpacing;
+            result.max.y += totalSpacing;
+        }
+        return result;
     }
 
     ~ASplitterLayout() override = default;
 
 private:
+    struct ResolvedItem {
+        bool visible = false;
+        int weight = 0;
+        int mainSize = 0;
+        int minMainSize = 0;
+        int maxMainSize = std::numeric_limits<int>::max();
+    };
+
     ASplitterHelper& mSplitterHelper;
+    AVector<_<AView>> mViews;
+    int mSpacing = 0;
+
+    template <typename T>
+    static T& getAxisValue(glm::tvec2<T>& v) {
+        return aui::layout_direction::getAxisValue(Direction, v);
+    }
+
+    template <typename T>
+    static T getAxisValue(const glm::tvec2<T>& v) {
+        return aui::layout_direction::getAxisValue(Direction, v);
+    }
+
+    template <typename T>
+    static T getPerpAxisValue(const glm::tvec2<T>& v) {
+        return aui::layout_direction::getPerpendicularAxisValue(Direction, v);
+    }
+
+    int computeStructuralMinimumMainAxisSize(const _<AView>& view, int perpendicularConstraint) const {
+        const int fixedSize = getAxisValue(view->getFixedSize());
+        if (fixedSize > 0) {
+            return fixedSize;
+        }
+
+        if constexpr (Direction == ALayoutDirection::HORIZONTAL) {
+            return view->computeMinMaxSizes(perpendicularConstraint).min.x;
+        } else {
+            return perpendicularConstraint == -1
+                ? view->computeMinMaxSizes().min.y
+                : view->measure(AConstraints::fixedWidth(perpendicularConstraint)).y;
+        }
+    }
+
+    int computeOverridenMainAxisSize(const _<AView>& view, AOptional<int> overridedSize, int perpendicularConstraint) const {
+        const int minimum = computeStructuralMinimumMainAxisSize(view, perpendicularConstraint);
+        if (overridedSize) {
+            return std::max(minimum, *overridedSize);
+        }
+
+        return -1;
+    }
+
+    int computePreferredMainAxisSize(const _<AView>& view, int perpendicularConstraint, bool expandingAsPreferred) const {
+        const auto mainOverride = computeOverridenMainAxisSize(view, findOverridedSize(view), perpendicularConstraint);
+        if (mainOverride != -1) {
+            return mainOverride;
+        }
+
+        const int expanding = getAxisValue(view->getExpanding());
+        if (expanding > 0 && !expandingAsPreferred) {
+            return computeMinimumMainAxisSize(view, perpendicularConstraint);
+        }
+
+        if constexpr (Direction == ALayoutDirection::HORIZONTAL) {
+            return perpendicularConstraint == -1
+                ? view->computeMinMaxSizes().max.x
+                : view->measure(AConstraints::fixedHeight(perpendicularConstraint)).x;
+        } else {
+            return perpendicularConstraint == -1
+                ? view->computeMinMaxSizes().max.y
+                : view->measure(AConstraints::fixedWidth(perpendicularConstraint)).y;
+        }
+    }
+
+    int computeMinimumMainAxisSize(const _<AView>& view, int perpendicularConstraint) const {
+        const auto mainOverride = computeOverridenMainAxisSize(view, findOverridedSize(view), perpendicularConstraint);
+        if (mainOverride != -1) {
+            return mainOverride;
+        }
+
+        return computeStructuralMinimumMainAxisSize(view, perpendicularConstraint);
+    }
+
+    int computePerpendicularSize(const _<AView>& view, int mainSize, int availablePerpendicular) const {
+        if constexpr (Direction == ALayoutDirection::HORIZONTAL) {
+            const int minimum = view->computeMinMaxSizes(mainSize).min.y;
+            if (availablePerpendicular != -1 &&
+                getPerpAxisValue(view->getExpanding()) != 0 &&
+                getPerpAxisValue(view->getFixedSize()) == 0) {
+                return std::max(minimum, availablePerpendicular);
+            }
+            return std::max(minimum, view->measure({
+                .minWidth = mainSize,
+                .maxWidth = mainSize,
+                .maxHeight = availablePerpendicular,
+            }).y);
+        } else {
+            const int minimum = view->computeMinMaxSizes(mainSize).min.x;
+            if (availablePerpendicular != -1 &&
+                getPerpAxisValue(view->getExpanding()) != 0 &&
+                getPerpAxisValue(view->getFixedSize()) == 0) {
+                return std::max(minimum, availablePerpendicular);
+            }
+            return std::max(minimum, view->measure({
+                .minHeight = mainSize,
+                .maxHeight = mainSize,
+                .maxWidth = availablePerpendicular,
+            }).x);
+        }
+    }
+
+    AOptional<int> findOverridedSize(const _<AView>& view) const {
+        for (const auto& item : mSplitterHelper.items()) {
+            if (item.view == view) {
+                return item.overridedSize;
+            }
+        }
+        return std::nullopt;
+    }
+
+    std::size_t countVisibleItems() const {
+        return std::count_if(mViews.begin(), mViews.end(), [](const _<AView>& view) {
+            return bool(view->getVisibility() & Visibility::FLAG_CONSUME_SPACE);
+        });
+    }
+
+    std::vector<ResolvedItem> resolveMainAxisSizes(
+        int availableMainSize,
+        int perpendicularConstraint,
+        bool bounded,
+        bool expandingAsPreferred) const {
+        std::vector<ResolvedItem> result;
+        result.reserve(mViews.size());
+
+        int occupiedMainSize = 0;
+        int visibleCount = 0;
+        for (const auto& view : mViews) {
+            ResolvedItem item;
+            if (!(view->getVisibility() & Visibility::FLAG_CONSUME_SPACE)) {
+                result.push_back(item);
+                continue;
+            }
+
+            item.visible = true;
+            item.minMainSize = computeMinimumMainAxisSize(view, perpendicularConstraint);
+
+            const int fixedSize = getAxisValue(view->getFixedSize());
+            if (fixedSize > 0) {
+                item.mainSize = fixedSize;
+                item.maxMainSize = fixedSize;
+            } else if (auto overridedSize = findOverridedSize(view)) {
+                item.mainSize = std::max(item.minMainSize, *overridedSize);
+                item.maxMainSize = item.mainSize;
+            } else {
+                item.weight = getAxisValue(view->getExpanding());
+                item.mainSize = computePreferredMainAxisSize(view, perpendicularConstraint, expandingAsPreferred);
+                const int maxSize = getAxisValue(view->getMaxSize());
+                item.maxMainSize = maxSize > 0 ? std::max(item.mainSize, maxSize) : std::numeric_limits<int>::max();
+                if (item.weight > 0 && !expandingAsPreferred) {
+                    item.mainSize = item.minMainSize;
+                }
+            }
+
+            occupiedMainSize += item.mainSize + getAxisValue(view->getMargin().occupiedSize());
+            ++visibleCount;
+            result.push_back(item);
+        }
+        occupiedMainSize += std::max(0, visibleCount - 1) * mSpacing;
+
+        if (bounded && occupiedMainSize > availableMainSize) {
+            int remainingToShrink = occupiedMainSize - availableMainSize;
+            for (auto it = result.rbegin(); it != result.rend() && remainingToShrink > 0; ++it) {
+                if (!it->visible) {
+                    continue;
+                }
+                const int shrinkCapacity = it->mainSize - it->minMainSize;
+                const int delta = std::min(shrinkCapacity, remainingToShrink);
+                it->mainSize -= delta;
+                remainingToShrink -= delta;
+            }
+        }
+
+        if (bounded) {
+            int remainingToExpand = availableMainSize;
+            for (std::size_t i = 0; i < mViews.size(); ++i) {
+                if (!result[i].visible) {
+                    continue;
+                }
+                remainingToExpand -= result[i].mainSize + getAxisValue(mViews[i]->getMargin().occupiedSize());
+            }
+            remainingToExpand -= std::max(0, visibleCount - 1) * mSpacing;
+
+            while (remainingToExpand > 0) {
+                int totalWeight = 0;
+                for (const auto& item : result) {
+                    if (item.visible && item.weight > 0 && item.mainSize < item.maxMainSize) {
+                        totalWeight += item.weight;
+                    }
+                }
+                if (totalWeight == 0) {
+                    break;
+                }
+
+                int distributed = 0;
+                for (auto& item : result) {
+                    if (!item.visible || item.weight == 0 || item.mainSize >= item.maxMainSize) {
+                        continue;
+                    }
+                    const int share = std::max(1, remainingToExpand * item.weight / totalWeight);
+                    const int delta = std::min(share, item.maxMainSize - item.mainSize);
+                    item.mainSize += delta;
+                    distributed += delta;
+                }
+                if (distributed == 0) {
+                    break;
+                }
+                remainingToExpand -= distributed;
+            }
+        }
+
+        return result;
+    }
 };
 }   // namespace
 
@@ -110,7 +440,7 @@ _<AView> ASplitter::Builder<Layout>::build() {
     splitter->setExpanding(mExpanding);
     splitter->mHelper.setDirection(Layout::DIRECTION);
 
-    splitter->setLayout(std::make_unique<ASplitterLayout<Layout>>(splitter->mHelper));
+    splitter->setLayout(std::make_unique<ASplitterLayout<Layout::DIRECTION>>(splitter->mHelper));
 
     bool atLeastOneItemHasExpanding = false;
     for (auto& item : mItems) {
