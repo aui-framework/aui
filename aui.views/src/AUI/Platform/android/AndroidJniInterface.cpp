efab1a46a266f7cd93fa14154f0258b04f081c5b
Nelonn <42481486+Nelonn@users.noreply.github.com>
2026-05-13 14:36:54 +0400

    fix build on android

--- aui.views/src/AUI/Platform/android/AndroidJniInterface.cpp
+++ aui.views/src/AUI/Platform/android/AndroidJniInterface.cpp
@@ -32,7 +32,7 @@ extern "C"
 JNIEXPORT void JNICALL
 Java_com_github_aui_android_AuiView_handleResize(JNIEnv *env, jclass clazz, jint width, jint height) {
     runOnGLThread([=] {
-        AUI_NULLSAFE(ASurface::current())->onResize({width, height});
+        AUI_NULLSAFE(ASurface::current())->onResize(width, height);
     });
 }
 
