5d274298dcd904504a5c2b02823a169726e847a2
Nelonn <42481486+Nelonn@users.noreply.github.com>
2026-05-30 23:54:54 +0400

    update

--- aui.views/src/AUI/GL/ShaderUniforms.h
+++ aui.views/src/AUI/GL/ShaderUniforms.h
@@ -9,35 +9,37 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
+#pragma once
+
 #include <AUI/GL/Program.h>
 
 namespace aui::ShaderUniforms {
-    extern gl::Program::Uniform COLOR;
-    extern gl::Program::Uniform COLOR1;
-    extern gl::Program::Uniform COLOR2;
-    extern gl::Program::Uniform GRADIENT_MAT_UV;
-    extern gl::Program::Uniform TRANSFORM;
-    extern gl::Program::Uniform SL_UNIFORM_TRANSFORM;
-    extern gl::Program::Uniform UV_SCALE;
-    extern gl::Program::Uniform SL_UNIFORM_SIGMA;
-    extern gl::Program::Uniform SL_UNIFORM_LOWER;
-    extern gl::Program::Uniform SL_UNIFORM_UPPER;
-    extern gl::Program::Uniform PIXEL_TO_UV;
-    extern gl::Program::Uniform OUTER_SIZE;
-    extern gl::Program::Uniform INNER_SIZE;
-    extern gl::Program::Uniform WINDOW_SIZE;
-    extern gl::Program::Uniform MASK;
-    extern gl::Program::Uniform USE_MASK;
-    extern gl::Program::Uniform OUTER_TEXEL_SIZE;
-    extern gl::Program::Uniform OUTER_TO_INNER;
-    extern gl::Program::Uniform RADIUS;
-    extern gl::Program::Uniform RECT_POS;
-    extern gl::Program::Uniform RECT_SIZE;
-    extern gl::Program::Uniform BORDER_WIDTH;
-    extern gl::Program::Uniform WHICH_ALGO;
-    extern gl::Program::Uniform M1;
-    extern gl::Program::Uniform M2;
-    extern gl::Program::Uniform DIVIDER;
-    extern gl::Program::Uniform THRESHOLD;
-    extern gl::Program::Uniform KERNEL;
+    extern ::gl::Program::Uniform COLOR;
+    extern ::gl::Program::Uniform COLOR1;
+    extern ::gl::Program::Uniform COLOR2;
+    extern ::gl::Program::Uniform GRADIENT_MAT_UV;
+    extern ::gl::Program::Uniform TRANSFORM;
+    extern ::gl::Program::Uniform SL_UNIFORM_TRANSFORM;
+    extern ::gl::Program::Uniform UV_SCALE;
+    extern ::gl::Program::Uniform SL_UNIFORM_SIGMA;
+    extern ::gl::Program::Uniform SL_UNIFORM_LOWER;
+    extern ::gl::Program::Uniform SL_UNIFORM_UPPER;
+    extern ::gl::Program::Uniform PIXEL_TO_UV;
+    extern ::gl::Program::Uniform OUTER_SIZE;
+    extern ::gl::Program::Uniform INNER_SIZE;
+    extern ::gl::Program::Uniform WINDOW_SIZE;
+    extern ::gl::Program::Uniform MASK;
+    extern ::gl::Program::Uniform USE_MASK;
+    extern ::gl::Program::Uniform OUTER_TEXEL_SIZE;
+    extern ::gl::Program::Uniform OUTER_TO_INNER;
+    extern ::gl::Program::Uniform RADIUS;
+    extern ::gl::Program::Uniform RECT_POS;
+    extern ::gl::Program::Uniform RECT_SIZE;
+    extern ::gl::Program::Uniform BORDER_WIDTH;
+    extern ::gl::Program::Uniform WHICH_ALGO;
+    extern ::gl::Program::Uniform M1;
+    extern ::gl::Program::Uniform M2;
+    extern ::gl::Program::Uniform DIVIDER;
+    extern ::gl::Program::Uniform THRESHOLD;
+    extern ::gl::Program::Uniform KERNEL;
 }
