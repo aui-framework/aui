5a70d975ddf4e54f010f9a9505bb73a5567be930
Nelonn <42481486+Nelonn@users.noreply.github.com>
2026-06-05 01:36:37 +0400

    update

--- aui.views/shaders/rounded.glsl
+++ aui.views/shaders/rounded.glsl
@@ -1,18 +1,12 @@
-float rounded(vec2 absolute, vec2 size, vec2 absoluteDerivatives) {
-    vec2 circleCenter = 1.0 - size;
-    
-#if defined(GL_ES) && !defined(GL_OES_standard_derivatives)
-    vec2 fw = vec2(0.01);
-#else
-    vec2 fw = absoluteDerivatives;
-#endif
+float sdRoundedBox(in vec2 p, in vec2 b, in float r) {
+    vec2 q = abs(p) - b + r;
+    return min(max(q.x, q.y), 0.0) + length(max(q, 0.0)) - r;
+}
 
-    vec2 safeFw = max(fw, 1e-7);
-    vec2 d_corner = (absolute - circleCenter) / safeFw;
-    float radius_pixels = (size.x / safeFw.x + size.y / safeFw.y) * 0.5;
-    
-    vec2 max_d = max(d_corner, 0.0);
-    float dist_pixels = length(max_d) + min(max(d_corner.x, d_corner.y), 0.0) - radius_pixels;
-    
-    return smoothstep(0.5, -0.5, dist_pixels);
+float rounded(vec2 absolute, vec2 size, vec2 absoluteDerivatives) {
+    vec2 fw = max(absoluteDerivatives, 1e-7);
+    vec2 halfSize = 1.0 / fw;
+    float r = size.x * halfSize.x;
+    float d = sdRoundedBox(absolute * halfSize, halfSize, r);
+    return smoothstep(0.5, -0.5, d);
 }
--- aui.views/shaders/shadow_inner.fsh.glsl
+++ aui.views/shaders/shadow_inner.fsh.glsl
@@ -5,7 +5,7 @@ in vec4 vColor;
 uniform vec2 lower;
 uniform vec2 upper;
 uniform float sigma;
-uniform vec2 outerSize;
+in vec2 vOuterSize;
 
 uniform sampler2D u_mask;
 uniform bool u_useMask;
@@ -20,30 +20,24 @@ vec4 erf(vec4 x) {
     return s - s / (x * x);
 }
 
-float rounded(vec2 absolute, vec2 size, vec2 absoluteDerivatives) {
-    vec2 circleCenter = 1.0 - size;
-    
-#if defined(GL_ES) && !defined(GL_OES_standard_derivatives)
-    vec2 fw = vec2(0.01);
-#else
-    vec2 fw = absoluteDerivatives;
-#endif
+float sdRoundedBox(in vec2 p, in vec2 b, in float r) {
+    vec2 q = abs(p) - b + r;
+    return min(max(q.x, q.y), 0.0) + length(max(q, 0.0)) - r;
+}
 
-    vec2 safeFw = max(fw, 1e-7);
-    vec2 d_corner = (absolute - circleCenter) / safeFw;
-    float radius_pixels = (size.x / safeFw.x + size.y / safeFw.y) * 0.5;
-    
-    vec2 max_d = max(d_corner, 0.0);
-    float dist_pixels = length(max_d) + min(max(d_corner.x, d_corner.y), 0.0) - radius_pixels;
-    
-    return smoothstep(0.5, -0.5, dist_pixels);
+float rounded(vec2 absolute, vec2 size, vec2 absoluteDerivatives) {
+    vec2 fw = max(absoluteDerivatives, 1e-7);
+    vec2 halfSize = 1.0 / fw;
+    float r = size.x * halfSize.x;
+    float d = sdRoundedBox(absolute * halfSize, halfSize, r);
+    return smoothstep(0.5, -0.5, d);
 }
 
 void main() {
     vec2 v = vVertex.xy;
     vec4 query = vec4(v - lower, v - upper);
     vec4 integral = 0.5 + 0.5 * erf(query * (sqrt(0.5) / sigma));
-    float factor = (1.0 - clamp((integral.z - integral.x) * (integral.w - integral.y), 0.0, 1.0)) * rounded(abs(vUv * 2.0 - 1.0), outerSize, fwidth(vUv) * 2.0);
+    float factor = (1.0 - clamp((integral.z - integral.x) * (integral.w - integral.y), 0.0, 1.0)) * rounded(abs(vUv * 2.0 - 1.0), vOuterSize, fwidth(vUv) * 2.0);
     fragColor = vColor * factor;
     if (u_useMask) {
         vec2 maskUv = (gl_FragCoord.xy - u_maskRect.xy) / u_maskRect.zw;
