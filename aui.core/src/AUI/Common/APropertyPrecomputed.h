bf9406fb7621fec338bb6d0276ee8416fddc5250
alex2772 <alex2772sc@gmail.com>
2026-09-16 04:07:40 +0300

    added version check

--- aui.core/src/AUI/Common/APropertyPrecomputed.h
+++ aui.core/src/AUI/Common/APropertyPrecomputed.h
@@ -209,27 +209,9 @@ struct aui::detail::ConnectionSourceTraits<aui::react::Expression<Expr>> {
  * referencing a data member (i.e. capturing `this`) fails to build. Copy the members into locals first:
  *
  * ```cpp
- * struct MyView {
- *   AProperty<float> value = 0;
- *   AProperty<float> min = 0;
- *   AProperty<float> max = 100;
- *   _<AView> operator()() {
+ * _<AView> operator()() {
  *     auto value = this->value, min = this->min, max = this->max; // copy contract::In (shared_ptr) into locals
  *     return Slider { .value = AUI_REACT((*value - *min) / (*max - *min)), ... };
- *   }
- * };
- * ```
- *
- * Another approach:
- *
- * ```cpp
- * struct State {
- *   AProperty<float> value = 0;
- *   AProperty<float> min = 0;
- *   AProperty<float> max = 100;
- * };
- * _<AView> myView(const _<State>& state) {
- *     return Slider { .value = AUI_REACT((*state->value - *state->min) / (*state->max - *state->min)), ... };
  * }
  * ```
  */
@@ -238,6 +220,13 @@ struct aui::detail::ConnectionSourceTraits<aui::react::Expression<Expr>> {
         _Pragma("clang diagnostic push")   \
         _Pragma("clang diagnostic error \"-Wdeprecated-this-capture\"")
 #    define AUI_REACT_NO_THIS_CAPTURE_POP _Pragma("clang diagnostic pop")
+#elif AUI_COMPILER_GCC && __GNUC__ >= 14
+// _Pragma inside macro bodies expanded in non-top-level contexts (lambdas, initializer lists) is a GCC bug
+// fixed in GCC 14 (https://gcc.gnu.org/bugzilla/show_bug.cgi?id=55578)
+#    define AUI_REACT_NO_THIS_CAPTURE_PUSH \
+        _Pragma("GCC diagnostic push")     \
+        _Pragma("GCC diagnostic error \"-Wdeprecated\"")
+#    define AUI_REACT_NO_THIS_CAPTURE_POP _Pragma("GCC diagnostic pop")
 #else
 #    define AUI_REACT_NO_THIS_CAPTURE_PUSH
 #    define AUI_REACT_NO_THIS_CAPTURE_POP
