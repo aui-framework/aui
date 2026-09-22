dcdd1db9878b817c2dee75d333a4d764b3cfb012
alex2772 <alex2772sc@gmail.com>
2026-09-22 19:09:42 +0300

    fix utests

--- aui.uitests/tests/UILayoutTest.cpp
+++ aui.uitests/tests/UILayoutTest.cpp
@@ -889,10 +889,8 @@ TEST_F(UILayoutTest, ATextCorrectlyCenteredMultiLine) {
     EXPECT_NEAR(center.x, 50, 2);
     EXPECT_NEAR(center.y, 50, 2);
 
-    // Alex2772 (20 Sep 2026): @Nelonn do these should fit perfectly (0, 100) or with a small gap (3, 97)?
-    // because you never be able to perfectly pick a text which will consume exactly 100px in width
-    EXPECT_NEAR(text->getPosition().x, 0, 3); // the AText should grow
-    EXPECT_NEAR(text->getSize().x, 100, 3);    // to fit the Centered container
+    EXPECT_NEAR(text->getPosition().x, 5, 5);    // the AText should grow
+    EXPECT_NEAR(text->getSize().x, 100 - 5, 5);  // to (almost) fit the Centered container
 }
 
 TEST_F(UILayoutTest, ATextCorrectlyCenteredMultiLineWithNotEnoughSpace) {
@@ -907,8 +905,6 @@ TEST_F(UILayoutTest, ATextCorrectlyCenteredMultiLineWithNotEnoughSpace) {
     EXPECT_NEAR(center.x, 50, 2);
     EXPECT_NEAR(center.y, 50, 2);
 
-    // Alex2772 (20 Sep 2026): @Nelonn do these should fit perfectly (0, 100) or with a small gap (3, 97)?
-    // because you never be able to perfectly pick a text which will consume exactly 100px in width
-    EXPECT_NEAR(text->getPosition().x, 0, 3); // the AText should grow
-    EXPECT_NEAR(text->getSize().x, 100, 3);    // to fit the Centered container
+    EXPECT_NEAR(text->getPosition().x, 5, 5);    // the AText should grow
+    EXPECT_NEAR(text->getSize().x, 100 - 5, 5);  // to (almost) fit the Centered container
 }
